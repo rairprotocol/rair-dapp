e84a4b2f0ca9388740860f696793eaf5f90e858d
Howard <492787221@qq.com>
2021-07-08 10:50:37 -0500

    minor bug fix and truffle-config added

--- rairMarketplace/smart_contracts/interfaces/IRAIR_ERC721_RairResaleRegistry.sol
+++ rairMarketplace/smart_contracts/interfaces/IRAIR_ERC721_RairResaleRegistry.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.4;
 import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 
 interface IRAIR_ERC721_RairResaleRegistry is IERC721{
-    function getTreasuryFundsCommissionSplits() external view returns (uint256[] memory _percentages, address payable[] memory _treasuries);
+    function getTreasuryFundsCommissionSplits() external view returns (uint256[] memory _percentages, address [] memory _treasuries);
     function getMaxTreasuryFundsCommission() external view returns (uint256);
 }
