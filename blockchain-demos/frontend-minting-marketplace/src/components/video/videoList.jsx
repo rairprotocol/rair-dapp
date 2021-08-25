a95220c023c2c1b6a2de2a9108f1c30efd8b4438
Juan Miguel Sanchez Mola <jmsm412@gmail.com>
2021-08-25 15:38:55 -0400

    Title Filter

--- blockchain-demos/frontend-minting-marketplace/src/components/video/videoList.jsx
+++ blockchain-demos/frontend-minting-marketplace/src/components/video/videoList.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
 import VideoItem from './videoItem.jsx';
+import InputField from '../common/InputField.jsx';
 
 const VideoList = props => {
 	const [mediaList, setMediaList] = useState([]);
+	const [titleSearch, setTitleSearch] = useState('');
 	const updateList = async () => {
 		let response = await (await fetch('/api/media/list', {
 			headers: {
@@ -23,7 +25,11 @@ const VideoList = props => {
 	}, [])
 
 	return <>
-		{Object.keys(mediaList).map((item, index) => {
+		<details className='col-12'>
+			<summary> Search </summary>
+			<InputField getter={titleSearch} setter={setTitleSearch} />
+		</details>
+		{Object.keys(mediaList).filter(item => mediaList[item].title.toLowerCase().includes(titleSearch.toLowerCase())).map((item, index) => {
 			return <VideoItem key={index} mediaList={mediaList} item={item} />
 		})}
 	</>
