ce90b31517826ebf51df6f5125c77a4303d879c4
valeriy-kovalev <valeriykovalyov.mail@gmail.com>
2021-09-02 13:46:47 +0300

    updating dev db script;

--- demo-decrypt-node/scripts/dev/2.1.0.mongo.js
+++ demo-decrypt-node/scripts/dev/2.1.0.mongo.js
@@ -1,3 +1,7 @@
+// Have to be run before rebuilding the server! Will remove the old version of the index
+db.Offer.dropIndex({ offerPool: 1, rangeIndex: 1 });
+db.Offer.dropIndex({ contract: 1, offerPool: 1, rangeIndex: 1 });
+
 // Renaming fields in MintedToken collection
 db.MintedToken.find().toArray().forEach((Token) => {
   const t = {};
