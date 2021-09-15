1ded90885d0425f46f1ef9582f1b28d5f1e6f4ca
Juan Miguel Sanchez Mola <jmsm412@gmail.com>
2021-09-15 16:42:18 -0400

    Factory query for product and offers

--- blockchain-demos/frontend-minting-marketplace/src/components/whitelabel/factory.jsx
+++ blockchain-demos/frontend-minting-marketplace/src/components/whitelabel/factory.jsx
@@ -24,6 +24,14 @@ const Factory = () => {
 						'x-rair-token': localStorage.token
 					}
 				})).json();
+				let response3 = await (await fetch(`/api/contracts/${contract.contractAddress}/products/offers`, {
+					headers: {
+						'x-rair-token': localStorage.token
+					}
+				})).json();
+				if (response3.success) {
+					response2.contract.products = response3.products
+				}
 				contractData.push(response2.contract)
 			} 
 			setContractArray(contractData);
@@ -39,10 +47,8 @@ const Factory = () => {
 	return <div className='w-100 text-start row mx-0 px-0'>
 		<h1>Your deployed contracts</h1>
 		{contractArray && contractArray.map((item, index) => {
-			return <div className='col-4 p-2'>
+			return <div className='col-4 p-2' key={index}>
 				<div style={{border: 'solid 1px black', borderRadius: '16px', position: 'relative'}} className='w-100 p-3'>
-					<h5>{item.title}</h5>
-					<small>({item.contractAddress})</small>
 					<abbr title={'Binance Testnet'}>
 						<img style={{
 							border: 'solid 1px black',
@@ -54,6 +60,52 @@ const Factory = () => {
 							maxHeight: '2rem'
 						}} src={images[item.blockchain]} />
 					</abbr>
+					<small>({item.contractAddress})</small>
+					<h5>{item.title}</h5>
+					{item.products.length} products!
+					<hr />
+					<div className='w-100 row px-0 mx-0'>
+						{item.products
+							.sort((a,b) => a.creationDate > b.creationDate ? 1 : -1)
+							.map((product, index) => {
+								return <div key={index} style={{position: 'relative'}} className='col-12 text-center'>
+									<div style={{position: 'absolute', top: 0, left: 0}}>
+										{product.firstTokenIndex}...
+									</div>
+									{product.name}<br />
+									<div style={{position: 'absolute', top: 0, right: 0}}>
+										...{product.firstTokenIndex + product.copies}
+									</div>
+									<progress
+										className='w-100'
+										max={product.firstTokenIndex + product.copies}
+										value={product.soldCopies}
+									/>
+									<div className='w-100 row px-0 mx-0'>
+										{product.offers
+											.sort((a,b) => a.creationDate > b.creationDate ? 1 : -1)
+											.map((offer, index) => {
+											return <div key={index} style={{position: 'relative'}} className='col-6 text-center'>
+												<div style={{position: 'absolute', top: 0, left: '1rem'}}>
+													{offer.range[0]}...
+												</div>
+												{offer.offerName}<br />
+												<div style={{position: 'absolute', top: 0, right: '1rem'}}>
+													...{offer.range[1]}
+												</div>
+												<progress
+													className='w-100'
+													max={offer.range[0]}
+													max={offer.range[1]}
+													value={offer.soldCopies}
+												/>
+											</div>
+										})}
+										<hr />
+									</div>
+								</div>
+						})}
+					</div>
 				</div>
 			</div>
 		})}
