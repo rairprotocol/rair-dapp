35eb63e317b0a9ff8daf05cd7c792e54bf7f7588
VincentBlau <christopher.rose.sys@gmail.com>
2021-09-16 22:52:57 -0700

    Cleanup

--- letsencrypt-docker-compose/cron/renew_certs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd /workdir
-echo "Renewing Let's Encrypt Certificates... (`date`)"
-docker-compose run --entrypoint certbot certbot renew
-echo "Reloading Nginx configuration"
-docker-compose exec -T nginx nginx -s reload
