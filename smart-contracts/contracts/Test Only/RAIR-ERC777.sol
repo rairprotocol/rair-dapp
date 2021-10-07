8b6ec23d201d3ad70758241724c6adc67d9b0dcc
Juan Miguel Sanchez Mola <jmsm412@gmail.com>
2021-10-07 12:56:32 -0400

    Artifacts and Clean Up

--- smart-contracts/contracts/Test Only/RAIR-ERC777.sol
+++ smart-contracts/contracts/Test Only/RAIR-ERC777.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.9; 
 import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
-import "hardhat/console.sol";
+//import "hardhat/console.sol";
 
 contract RAIR777 is ERC777 {
     constructor(uint256 initialSupply, address[] memory _defaultOperators )
