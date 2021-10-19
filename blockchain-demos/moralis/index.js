6ca9bba0da1a04a029897bd841d9c32e919c8956
Juan Miguel Sanchez Mola <jmsm412@gmail.com>
2021-10-19 15:45:01 -0400

    Get OfferPools and Offers

--- blockchain-demos/moralis/index.js
+++ blockchain-demos/moralis/index.js
@@ -24,7 +24,102 @@ const getABIData = (abi, type, eventName) => {
 	return {abi: resultingAbi, topic};
 };
 
+const getAppendedRanges = async (minterAddress, chainName) => {
+	await Moralis.Cloud.run(blockchainData[chainName].watchFunction, {
+		address: minterAddress,
+		'sync_historical': true
+	});
+
+	const {abi, topic} = getABIData(minterAbi, 'event', 'AppendedRange');
+	const options = {
+		address: minterAddress,
+		chain: blockchainData[chainName].chainId,
+		topic,
+		abi
+	}
+	let events = await Moralis.Web3API.native.getContractEvents(options);
+	events.result.forEach(async result => {
+		const Offer = Moralis.Object.extend("Offer");
+		const offerQuery = new Moralis.Query(Offer);
+		offerQuery.equalTo('transactionHash', result.transaction_hash);
+		const offerResult = await offerQuery.find();
+		if (offerResult.length === 0) {
+
+			const offer = new Offer();
+			offer.set('transactionHash', result.transaction_hash);
+			offer.set('blockchain', blockchainData[chainName].chainId);
+			offer.set('minterAddress', result.address);
+
+			offer.set('offerIndex', result.data.rangeIndex);
+			offer.set('contract', result.data.contractAddress);
+			offer.set('product', result.data.productIndex);
+			offer.set('offerPool', result.data.offerIndex);
+			offer.set('copies', (Ethers.BigNumber.from(result.data.endToken).sub(result.data.startToken)).toString());
+			offer.set('soldCopies', 0);
+			offer.set('sold', false);
+			offer.set('price', result.data.price);
+			offer.set('range', [result.data.startToken, result.data.endToken]);
+			offer.set('name', result.data.name);
+
+			await offer.save();
+			console.log(`[${chainName}] Saved Offer #${result.data.rangeIndex} of pool ${result.data.offerIndex}`);
+
+			const OfferPool = Moralis.Object.extend("OfferPool");
+			const offerPoolQuery = new Moralis.Query(OfferPool);
+			offerPoolQuery.equalTo('minterAddress', result.address);
+			offerPoolQuery.equalTo('blockchain', blockchainData[chainName].chainId);
+			offerPoolQuery.equalTo('marketplaceCatalogIndex', result.data.offerIndex);
+			const [offerPoolResult] = await offerPoolQuery.find();
+
+			if (offerPoolResult && offerPoolResult.get('transactionHash') !== result.transaction_hash) {
+				offerPoolResult.set('rangeNumber', (Ethers.BigNumber.from(offerPoolResult.get('rangeNumber')).add(1)).toString());
+				console.log(`[${chainName}] Updated offer count of pool ${offerPoolResult.get('marketplaceCatalogIndex')}`);
+				await offerPoolResult.save();
+			}
+		}
+	})
+};
+
+const getOfferPools = async (minterAddress, chainName) => {
+	await Moralis.Cloud.run(blockchainData[chainName].watchFunction, {
+		address: minterAddress,
+		'sync_historical': true
+	});
+
+	const {abi, topic} = getABIData(minterAbi, 'event', 'AddedOffer');
+	const options = {
+		address: minterAddress,
+		chain: blockchainData[chainName].chainId,
+		topic,
+		abi
+	}
+	let events = await Moralis.Web3API.native.getContractEvents(options);
+	events.result.forEach(async result => {
+		const OfferPool = Moralis.Object.extend("OfferPool");
+		const offerPoolQuery = new Moralis.Query(OfferPool);
+		offerPoolQuery.equalTo('transactionHash', result.transaction_hash);
+		const offerPoolResult = await offerPoolQuery.find();
+		if (offerPoolResult.length === 0) {
+			const offerPool = new OfferPool();
+			offerPool.set('transactionHash', result.transaction_hash);
+			offerPool.set('blockchain', blockchainData[chainName].chainId);
+			offerPool.set('minterAddress', result.address);
+			offerPool.set('marketplaceCatalogIndex', result.data.catalogIndex);
+			offerPool.set('contract', result.data.contractAddress);
+			offerPool.set('product', result.data.productIndex);
+			offerPool.set('rangeNumber', result.data.rangesCreated);
+			await offerPool.save();
+			console.log(`[${chainName}] Saved Offer Pool #${result.data.catalogIndex} of ${result.address}`);
+		}
+	})
+}
+
 const getDeployedContracts = async (factoryAddress, chainName) => {
+	await Moralis.Cloud.run(blockchainData[chainName].watchFunction, {
+		address: factoryAddress,
+		'sync_historical': true
+	});
+
 	const {abi, topic} = getABIData(factoryAbi, 'event', 'NewContractDeployed');
 	const options = {
 		address: factoryAddress,
@@ -134,6 +229,22 @@ const main = async () => {
 
 
 	/*
+	Offer
+	
+
+	MintedToken
+	token: { type: Number, required: true },
+	uniqueIndexInContract: { type: Number, required: true },
+	ownerAddress: { type: String, lowercase: true },
+	offerPool: { type: Number, required: true },
+	offer: { type: Number, required: true },
+	contract: { type: String, lowercase: true, required: true },
+	metadata: { type: Metadata, default: () => ({}) },
+	metadataURI: { type: String, default: 'none' },
+	authenticityLink: { type: String, default: 'none' },
+	isMinted: { type: Boolean, required: true },
+	creationDate: { type: Date, default: Date.now }
+
 	console.log('Minter');
 	let events = await Moralis.Web3API.native.getContractEvents(options)
 		.catch(err => console.error(err.error));
@@ -149,9 +260,13 @@ const main = async () => {
 	//getABIData(erc721Abi, 'event', 'ProductCreated');
 
 	// Queries a factory and stores all deployed contracts
-	await getDeployedContracts(blockchainData.mumbai.factoryAddress, 'mumbai');
+	//await getDeployedContracts(blockchainData.mumbai.factoryAddress, 'mumbai');
 	// Gets the Products from all Deployed Contracts
-	await getProducts('mumbai');
+	//await getProducts('mumbai');
+	// Gets the Offers (OfferPool) created
+	//await getOfferPools(blockchainData.mumbai.minterAddress ,'mumbai');
+	// Gets the ranges appended on the minter marketplace
+	//await getAppendedRanges(blockchainData.mumbai.minterAddress ,'mumbai');
 }
 
 try {
