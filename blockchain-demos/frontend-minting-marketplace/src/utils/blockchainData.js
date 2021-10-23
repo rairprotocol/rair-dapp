1dec173292a0522d651baf63318146dd80c9cbab
rairtech_devops <chris@rair.tech>
2021-10-23 12:55:29 -0700

    Update blockchain data

--- blockchain-demos/frontend-minting-marketplace/src/utils/blockchainData.js
+++ blockchain-demos/frontend-minting-marketplace/src/utils/blockchainData.js
@@ -6,7 +6,7 @@ const chainData = {
 	'0x61': {image: BinanceDiamond, name: 'Binance Testnet', chainId: '0x61'},
 	'0x13881': {image: MaticLogo, name: 'Matic(Polygon) Testnet', chainId: '0x13881'},
 	'0x5': {image: EthereumLogo, name: 'Ethereum Goerli', chainId: '0x5'},
-	'0x137': {image: MaticLogo, name: 'Matic(Polygon) Mainnet', chainId: '0x137'}
+	'0x89': {image: MaticLogo, name: 'Matic(Polygon) Mainnet', chainId: '0x89'}
 }
 
 export default chainData;
