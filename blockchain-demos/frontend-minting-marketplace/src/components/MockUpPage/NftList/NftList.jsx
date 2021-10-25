255984705788b4124cf9231cb3cc87797e40b109
Arsenii Pilipenko <apilipenko@vp.software>
2021-10-25 18:55:35 +0300

    added properties

--- blockchain-demos/frontend-minting-marketplace/src/components/MockUpPage/NftList/NftList.jsx
+++ blockchain-demos/frontend-minting-marketplace/src/components/MockUpPage/NftList/NftList.jsx
@@ -5,34 +5,38 @@ import thirdPict from '../assets/Graphics-WEB-2021-03.png'
 import NftItem from './NftItem'
 
 const arrPic = [
-    firstPict , secondPict , thirdPict
+    firstPict, secondPict, thirdPict
 ]
 
-const NftList = ({data, primaryColor, textColor}) => {
+const NftList = ({ data, primaryColor, textColor }) => {
     return (
         <div className={'list-button-wrapper'}>
             {
                 data?.map((pict, index) => {
-                    if(pict.cover !== 'none'){
-                        return(<NftItem onClick={() => console.log('nftList',data)} 
-                            key={index} 
-                            pict={pict.cover} 
+                    if (pict.cover !== 'none') {
+                        return (<NftItem onClick={() => console.log('nftList', data)}
+                            key={`${pict.id}-${pict.productId}`}
+                            pict={pict.cover}
+                            allData={pict}
                             contractName={pict.contract}
                             price={pict.offerData.map(p => p.price)}
                             primaryColor={primaryColor}
                             textColor={textColor}
-                            collectionIndexInContract={pict.collectionIndexInContract}/>)
+                            blockchain={pict.blockchain}
+                            collectionName={pict.name}
+                            ownerCollectionUser={pict.user}
+                            collectionIndexInContract={pict.collectionIndexInContract} />)
                     } else {
                         return null
                         // return(<img width="291" height="291" key={index} src={firstPict} alt="first" />)
                     }
-                
+
                 })
             }
-                <img className={'col-12 col-sm-6 col-md-4 col-lg-3 px-1 text-start pictures-wrapper'} width="291" height="291" src={firstPict} alt="first" />
-                <img className={'col-12 col-sm-6 col-md-4 col-lg-3 px-1 text-start pictures-wrapper'}  width="291" height="291" src={secondPict} alt="first" />
-                <img className={'col-12 col-sm-6 col-md-4 col-lg-3 px-1 text-start pictures-wrapper'}  width="291" height="291" src={thirdPict} alt="first" />
-        </div> 
+            <img className={'col-12 col-sm-6 col-md-4 col-lg-3 px-1 text-start pictures-wrapper'} width="291" height="291" src={firstPict} alt="first" />
+            <img className={'col-12 col-sm-6 col-md-4 col-lg-3 px-1 text-start pictures-wrapper'} width="291" height="291" src={secondPict} alt="first" />
+            <img className={'col-12 col-sm-6 col-md-4 col-lg-3 px-1 text-start pictures-wrapper'} width="291" height="291" src={thirdPict} alt="first" />
+        </div>
     )
 }
 
