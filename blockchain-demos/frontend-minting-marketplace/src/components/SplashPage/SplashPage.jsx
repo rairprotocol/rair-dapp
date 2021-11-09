aea897a7fec62a437698bf63f9f48c6c9d8e4dad
Arsenii Pilipenko <apilipenko@vp.software>
2021-11-09 13:18:29 +0200

    Nispley Splash page is done like static

--- blockchain-demos/frontend-minting-marketplace/src/components/SplashPage/SplashPage.jsx
+++ blockchain-demos/frontend-minting-marketplace/src/components/SplashPage/SplashPage.jsx
@@ -8,6 +8,10 @@ import Nft_1 from './images/special-offer_1.jpeg';
 import Nft_2 from './images/special-offer_3.jpeg';
 import Nft_3 from './images/img-left-tokens.jpeg';
 import NftImage from './images/nft-img.jpeg';
+import UnlockableVideo from './images/unlockbleVideo.png';
+import Teammate_1 from './images/teammate_1.png';
+import Teammate_2 from './images/teammate_2.png';
+import JoinCommunity from './images/community.png';
 
 const SplashPage = () => {
     return (
@@ -121,6 +125,226 @@ const SplashPage = () => {
                 <div className="title-gets">
                     <h3>What <span className="text-gradient">you</span> get?</h3>
                 </div>
+                <div className="block-videos">
+                    <div className="box-video">
+                        <div className="video-locked">
+                            <div style={{ position: "relative" }}>
+                                <div className="video-icon">
+                                    <i className="fa fa-lock" aria-hidden="true"></i>
+                                    <p>Locked</p>
+                                </div>
+                                <img src={UnlockableVideo} alt="unlockble video" />
+                            </div>
+                            <div className="video-description">
+                                <div>
+                                    <p>Song 1</p>
+                                </div>
+                                <div className="video-timer">
+                                    <p>00:03:23</p>
+                                </div>
+                                <div className="video-key">
+                                    <i class="fas fa-key"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="box-video">
+                        <div className="video-locked">
+                            <div style={{ position: "relative" }}>
+                                <div className="video-icon">
+                                    <i className="fa fa-lock" aria-hidden="true"></i>
+                                    <p>Locked</p>
+                                </div>
+                                <img src={UnlockableVideo} alt="unlockble video" />
+                            </div>
+                            <div className="video-description">
+                                <div>
+                                    <p>Song 1</p>
+                                </div>
+                                <div className="video-timer">
+                                    <p>00:03:23</p>
+                                </div>
+                                <div className="video-key">
+                                    <i class="fas fa-key"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="box-video">
+                        <div className="video-locked">
+                            <div style={{ position: "relative" }}>
+                                <div className="video-icon">
+                                    <i className="fa fa-lock" aria-hidden="true"></i>
+                                    <p>Locked</p>
+                                </div>
+                                <img src={UnlockableVideo} alt="unlockble video" />
+                            </div>
+                            <div className="video-description">
+                                <div>
+                                    <p>Song 1</p>
+                                </div>
+                                <div className="video-timer">
+                                    <p>00:03:23</p>
+                                </div>
+                                <div className="video-key">
+                                    <i class="fas fa-key"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="box-video">
+                        <div className="video-locked">
+                            <div style={{ position: "relative" }}>
+                                <div className="video-icon">
+                                    <i className="fa fa-lock" aria-hidden="true"></i>
+                                    <p>Locked</p>
+                                </div>
+                                <img src={UnlockableVideo} alt="unlockble video" />
+                            </div>
+                            <div className="video-description">
+                                <div>
+                                    <p>Song 1</p>
+                                </div>
+                                <div className="video-timer">
+                                    <p>00:03:23</p>
+                                </div>
+                                <div className="video-key">
+                                    <i class="fas fa-key"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="box-video">
+                        <div className="video-locked">
+                            <div style={{ position: "relative" }}>
+                                <div className="video-icon">
+                                    <i className="fa fa-lock" aria-hidden="true"></i>
+                                    <p>Locked</p>
+                                </div>
+                                <img src={UnlockableVideo} alt="unlockble video" />
+                            </div>
+                            <div className="video-description">
+                                <div>
+                                    <p>Song 1</p>
+                                </div>
+                                <div className="video-timer">
+                                    <p>00:03:23</p>
+                                </div>
+                                <div className="video-key">
+                                    <i class="fas fa-key"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="box-video">
+                        <div className="video-locked">
+                            <div style={{ position: "relative" }}>
+                                <div className="video-icon">
+                                    <i className="fa fa-lock" aria-hidden="true"></i>
+                                    <p>Locked</p>
+                                </div>
+                                <img src={UnlockableVideo} alt="unlockble video" />
+                            </div>
+                            <div className="video-description">
+                                <div>
+                                    <p>Song 1</p>
+                                </div>
+                                <div className="video-timer">
+                                    <p>00:03:23</p>
+                                </div>
+                                <div className="video-key">
+                                    <i class="fas fa-key"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="splash-team">
+                <div className="title-team">
+                    <h3>Meet <span className="text-gradient">the</span> Team</h3>
+                </div>
+                <div className="meet-team">
+                    <div className="box-teammate">
+                        <div className="img-teammate">
+                            <img src={Teammate_1} alt="photo" />
+                        </div>
+                        <div className="position-teammate">
+                            <h4>Frank Castle • Position</h4>
+                            <div className="teammate-description">
+                                <p>
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                                    anim id est laborum. Duis aute irure dolor in reprehenderit in
+                                    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                                    officia deserunt mollit anim id est laborum. Duis aute irure dolor
+                                    in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                                    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                                    in culpa qui officia deserunt mollit anim id est laborum. Duis aute
+                                    irure dolor in reprehenderit in voluptate velit esse cillum.
+                                </p>
+                            </div>
+                            <div className="box-socials">
+                                <span className="text-gradient"><i class="fab fa-twitter"></i></span>
+                                <span className="text-gradient"><i class="fab fa-linkedin-in"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="box-teammate">
+                        <div className="img-teammate">
+                            <img src={Teammate_2} alt="photo" />
+                        </div>
+                        <div className="position-teammate">
+                            <h4>Dylan Spikes • Position</h4>
+                            <div className="teammate-description">
+                                <p>
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                                    anim id est laborum. Duis aute irure dolor in reprehenderit in
+                                    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                                    officia deserunt mollit anim id est laborum. Duis aute irure dolor
+                                    in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                                    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                                    in culpa qui officia deserunt mollit anim id est laborum. Duis aute
+                                    irure dolor in reprehenderit in voluptate velit esse cillum.
+                                </p>
+                            </div>
+                            <div className="box-socials">
+                                <span className="text-gradient"><i class="fab fa-twitter"></i></span>
+                                <span className="text-gradient"><i class="fab fa-linkedin-in"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="join-community">
+                <div className="title-join">
+                    <h3>Join <span className="text-gradient">the</span> community</h3>
+                </div>
+                <div className="community-description">
+                    <div className="community-text">
+                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                            anim id est laborum. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                            sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                            mollit anim id est laborum. Duis aute irure dolor.
+                        </p>
+
+                        <div className="btn-buy-metamask">
+                            <button>Join with Telegram</button>
+                        </div>
+                    </div>
+                    <div className="join-pic">
+                        <img src={JoinCommunity} alt="photo-join" />
+                    </div>
+                </div>
+
             </div>
         </div>
     )
