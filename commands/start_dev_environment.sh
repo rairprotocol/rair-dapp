810d7f46937f0d9f2cacb8bbe9da34523f0a44fc
Brian Fogg <fogg4444@gmail.com>
2021-12-08 13:07:30 -0800

    cleanup

--- commands/start_dev_environment.sh
+++ commands/start_dev_environment.sh
@@ -4,5 +4,7 @@ echo "Starting dev environment..."
 
 DIR=$(dirname $0)
 
-docker-compose -f $DIR/../docker-compose.local.yml build
-docker-compose -f $DIR/../docker-compose.local.yml up
+DOCKER_COMPOSE_YML=$DIR/../docker-compose.local.yml
+
+docker-compose -f $DOCKER_COMPOSE_YML build
+docker-compose -f $DOCKER_COMPOSE_YML up
