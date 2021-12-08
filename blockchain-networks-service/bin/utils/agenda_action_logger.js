1942e02fe98db71d7654808afd1a34a45d6c20d3
Brian Fogg <fogg4444@gmail.com>
2021-12-08 14:48:46 -0800

    modified logger

--- blockchain-networks-service/bin/utils/agenda_action_logger.js
+++ blockchain-networks-service/bin/utils/agenda_action_logger.js
@@ -3,5 +3,5 @@ const log = require('./logger')(module);
 exports.logAgendaActionStart = ({
   agendaDefinition
 }) => {
-  log.info(`agenda action started execution: ${agendaDefinition}`);
+  log.info(`Agenda action started: ${agendaDefinition}`);
 };
