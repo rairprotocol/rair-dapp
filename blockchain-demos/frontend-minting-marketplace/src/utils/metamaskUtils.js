60a91690e815cb2eb4b3833c060f877aa439f6b4
Juan Miguel Sanchez Mola <jmsm@gmail.com>
2021-12-15 10:43:51 -0400

    Wrap requests in the creator views

---
+++ blockchain-demos/frontend-minting-marketplace/src/utils/metamaskUtils.js
@@ -0,0 +1,16 @@
+import Swal from 'sweetalert2';
+
+const metamaskCall = async (transaction) => {
+	try {
+		let paramsValidation = await transaction;
+		//await (paramsValidation).wait();
+		return true;
+	} catch (e) {
+		console.error(e);
+		let cleanError = e?.toString()?.split('"message":"execution reverted: ')?.at(1)?.split('"')?.at(0);
+		Swal.fire('Error', cleanError ? cleanError : e?.message, 'error');
+		return false;
+	}
+}
+
+export { metamaskCall };
