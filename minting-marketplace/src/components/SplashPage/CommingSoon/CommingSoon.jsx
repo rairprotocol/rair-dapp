3ac89381a2931834c6eabd84a60b8b820646794e
rairtech_devops <chris@rair.tech>
2022-01-01 10:25:31 -0800

    Restructuring for organizational sensibility

---
+++ minting-marketplace/src/components/SplashPage/CommingSoon/CommingSoon.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import DiscordIcon from '../images/discord-icon.png';
+import logoAuthor from '../images/colab.png';
+import { useHistory } from 'react-router-dom';
+
+const ComingSoon = () => {
+    const history = useHistory();
+
+    const goBack = () => {
+        history.goBack();
+    }
+
+    return (
+        <div className="col-12">
+            <div className="nipsey-comming-soon">
+                <div className="information-author">
+                    <div className="block-splash">
+                        <div className="text-splash">
+                            <div className="title-splash nipsey">
+                                <span>Coming soon</span>
+                            </div>
+                            <div className="text-description">
+                                <div>
+                                    Marketplace will be live after the official launch. In the meantime.
+                                </div>
+                            </div>
+                            <div className="release-join-discord">
+                                <div className="btn-discord">
+                                    <a href="https://discord.gg/NFeGnPkbfd" target="_blank"><img src={DiscordIcon} alt="discord icon" /> Join our Discord</a>
+                                </div>
+                            </div>
+                            <div className="btn-back-coming">
+                                <button onClick={goBack}>Back</button>
+                            </div>
+                            <div className="logo-author">
+                                {/* <img src={logoDigital} alt="southwest digital" /> */}
+                                <img src={logoAuthor} alt="logo-author" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ComingSoon
---
+++ minting-marketplace/src/components/SplashPage/UnlockVideos/UnlockVideoItem.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Metamask from "../images/metamask_logo.png";
+
+const UnlockVideoItem = ({typeVideo, nameVideo, timeVideo, UnlockableVideo, locked, primaryColor}) => {
+    return (
+        <div className="box-video">
+            <div
+                className="video-locked"
+                style={{ background: `${primaryColor === "rhyno" ? "#fff" : "#4E4D4DCC"}` }}
+            >
+                <div className="block-with-video" style={{ position: "relative" }}>
+                    <div className='video-block-metamask-some'>
+                        <img src={Metamask} alt="metamask logo" />
+                    </div>
+                    <img className="block-img-bg" src={UnlockableVideo} alt="unlockble video" />
+                </div>
+                <div className="video-description">
+                    <div className="video-title">
+                        <p style={{ color: `${primaryColor === "rhyno" ? "#000" : "#fff"}` }}>{nameVideo}</p>
+                    </div>
+                    <div className="video-timer">
+                        <p style={{ color: `${primaryColor === "rhyno" ? "#000" : "#A7A6A6"}` }}>{timeVideo}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UnlockVideoItem
