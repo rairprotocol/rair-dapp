21829df056f4854fe010ac62576115be88edcf52
valeriy-kovalev <valeriykovalyov.mail@gmail.com>
2022-01-04 19:16:19 +0200

    refactoring an endpoint for upload;

--- rairnode/bin/schemas/uploadVideo.js
+++ rairnode/bin/schemas/uploadVideo.js
@@ -11,7 +11,7 @@ module.exports = Joi.object({
     .required(),
   contract: Joi.string().required(),
   product: Joi.number().required(),
-  offer: Joi.array().min(1).items(Joi.number()).required(),
+  offer: Joi.array().items(Joi.number()),
   category: Joi.string().min(1).required(),
   demo: Joi.boolean()
 });
