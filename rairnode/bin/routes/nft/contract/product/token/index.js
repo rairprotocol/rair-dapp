fdb4db2a4f013a42ae7bcec192f8bffcb54efff8
valeriy-kovalev <valeriykovalyov.mail@gmail.com>
2022-01-11 19:44:06 +0200

    add edpoint fot pinning metadata to ipfs; update docs;

--- rairnode/bin/routes/nft/contract/product/token/index.js
+++ rairnode/bin/routes/nft/contract/product/token/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const _ = require('lodash');
 const { JWTVerification, validation } = require('../../../../../middleware');
+const { addMetadata, addPin } = require('../../../../../integrations/ipfsService')();
 
 module.exports = context => {
   const router = express.Router();
@@ -49,6 +50,46 @@ module.exports = context => {
       }, fieldsForUpdate, { new: true });
 
       return res.json({ success: true, token: updatedToken });
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+  // Pin metadata to pinata cloud
+  router.get('/pinning', JWTVerification(context), async (req, res, next) => {
+    try {
+      const { contract, offerPool, token } = req;
+      const { user } = req;
+      let metadataURI = 'none';
+
+      if (user.publicAddress !== contract.user) {
+        return res.status(403).send({
+          success: false,
+          message: `You have no permissions for updating token ${ token }.`
+        });
+      }
+
+      const foundToken = await context.db.MintedToken.findOne({
+        contract: contract._id,
+        offerPool: offerPool.marketplaceCatalogIndex,
+        token
+      });
+
+      if (_.isEmpty(foundToken)) {
+        return res.status(400).send({ success: false, message: 'Token not found.' });
+      }
+
+      metadataURI = foundToken.metadataURI;
+
+      if (!_.isEmpty(foundToken.metadata)) {
+        const CID = await addMetadata(foundToken.metadata, _.get(foundToken.metadata, 'name', 'none'));
+        await addPin(CID, `metadata_${ _.get(foundToken.metadata, 'name', 'none') }`);
+        metadataURI = `${ process.env.PINATA_GATEWAY }/${ CID }`;
+      }
+
+      await foundToken.updateOne({ metadataURI });
+
+      return res.json({ success: true, metadataURI });
     } catch (err) {
       return next(err);
     }
