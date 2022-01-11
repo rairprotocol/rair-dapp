68eb1be59e519b165eff7a494637aae628ba3d60
Brian Fogg <fogg4444@gmail.com>
2022-01-11 15:16:25 -0800

    adding tf admin json files as variables to auth TF Cloud `

--- terraform/environments/staging/main.tf
+++ terraform/environments/staging/main.tf
@@ -8,6 +8,16 @@ terraform {
   }
 }
 
+variable "gcp_auth_file" {
+  type        = string
+  description = "GCP authentication file"
+}
+
+provider "google" {
+  credentials = file(var.gcp_auth_file)
+  project     = "rair-market-staging"
+}
+
 module "foundation" {
   source = "../../modules/foundation"
 
