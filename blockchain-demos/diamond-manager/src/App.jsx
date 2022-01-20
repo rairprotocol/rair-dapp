1115d96c58ac0f3ae2e8501c859434bdc1c3639a
Juan Miguel Sanchez Mola <jmsm412@gmail.com>
2022-01-20 10:06:24 -0400

    Improved Diamond Manager

--- blockchain-demos/diamond-manager/src/App.jsx
+++ blockchain-demos/diamond-manager/src/App.jsx
@@ -1,100 +1,133 @@
-import {useState, useEffect, useCallback} from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 import * as ethers from 'ethers';
-// Standard Diamond Facets
-import * as DiamondCutFacet from './contractAbis/0x5/DiamondCutFacet.json'; 
-import * as DiamondLoupeFacet from './contractAbis/0x5/DiamondLoupeFacet.json';
-import * as OwnershipFacet from './contractAbis/0x5/OwnershipFacet.json';
-// Main Diamond
-import * as FactoryDiamond from './contractAbis/0x5/FactoryDiamond.json';
-// Factory
-import * as ERC777ReceiverFacet from './contractAbis/0x5/ERC777ReceiverFacet.json';
-import * as creatorFacet from './contractAbis/0x5/creatorFacet.json';
-import * as TokensFacet from './contractAbis/0x5/TokensFacet.json';
-// Token Facets
-import * as ERC721Facet from './contractAbis/0x5/ERC721Facet.json';
-import * as RAIRMetadataFacet from './contractAbis/0x5/RAIRMetadataFacet.json';
-import * as RAIRProductFacet from './contractAbis/0x5/RAIRProductFacet.json';
-import * as RAIRRangesFacet from './contractAbis/0x5/RAIRRangesFacet.json';
 
 import {getSelectors} from './utils/selectorUtils.js';
+
+// Import order is relevant and important, don't rearrange!
+const facetNames = [
+	'FactoryDiamond', // Main Factory Diamond
+	'DiamondCutFacet', // Standard Diamond Functions
+	'DiamondLoupeFacet',
+	'OwnershipFacet',
+	'ERC777ReceiverFacet', // Factory Facets
+	'creatorFacet',
+	'TokensFacet',
+	'ERC721Facet', // Deployed Token Facets
+	'RAIRMetadataFacet',
+	'RAIRProductFacet',
+	'RAIRRangesFacet'
+]
 
 const FacetCutAction_ADD = 0;
 const FacetCutAction_REPLACE = 1;
 const FacetCutAction_REMOVE = 2;
 
-const ContractData = ({FacetData, mainDiamond, setMainDiamond, signer, provider, usedSelectors, setUsedSelectors, setQueriedFacets, queriedFacets}) => {
-	const {address, devdoc, abi} = FacetData;
+const ContractData = ({
+	connectMainDiamondFactory,
+	FacetData,
+	mainDiamond,
+	setMainDiamond,
+	signer,
+	provider,
+	usedSelectors,
+	setUsedSelectors,
+	setQueriedFacets,
+	queriedFacets
+}) => {
+	const {address, abi} = FacetData;
 	const [selectorsInDiamond, setSelectorsInDiamond] = useState();
 	const [functions, setFunctions] = useState();
-	const [contractInstance, setContractInstance] = useState();
 
 	useEffect(() => {
 		let facetInstance = new ethers.Contract(address, abi, signer);
-		setContractInstance(facetInstance)
-		setFunctions(Object.keys(facetInstance.functions).filter(item => item.includes('(')));
 		let [facetData] = queriedFacets.filter(item => item.facetAddress === address);
 		setSelectorsInDiamond(facetData);
+		setFunctions(Object.keys(facetInstance.functions)
+			.filter(item => item.includes('('))
+			.map(item => {
+				let hash = facetInstance.interface.getSighash(item);
+				return {
+					hashed: hash,
+					signatureInDiamond: usedSelectors.includes(hash),
+					functionVersionInDiamond: facetData?.functionSelectors.includes(hash),
+					name: item,
+					selected: true
+				}
+			}));
 		if (facetData !== undefined) {
 
 		}
-	}, [queriedFacets, usedSelectors]);
+	}, [queriedFacets, usedSelectors, abi, address, signer]);
+
+	const updateSelected = (index) => {
+		let aux = [...functions];
+		aux[index].selected = !aux[index].selected;
+		setFunctions(aux);
+	}
+
+	const callItemCuts = async (FacetCutAction) => {
+		const cutItem = {
+			facetAddress: FacetCutAction === FacetCutAction_REMOVE ? ethers.constants.AddressZero : address,
+			action: FacetCutAction,
+			functionSelectors: functions.filter(item => item.selected).map(item => item.hashed)
+			//getSelectors(facetInstance, aux).filter(item => !usedSelectors.includes(item))
+		}
+		await mainDiamond.diamondCut(
+			[cutItem], ethers.constants.AddressZero, ethers.utils.toUtf8Bytes('')
+		);
+	}
 
 	return <div className='col-12 col-md-6 mb-5'>
 		Contract address: <b>{address}</b>
-		{functions && Object.keys(functions).length > 0 && <>
+		{functions && functions.length > 0 && <>
 			<hr/>
 			Functions:
 			<ul className='w-100'>
-				{functions && functions.map((methodName, index) => {
-					let hashed = contractInstance.interface.getSighash(methodName);
+				{functions.map((method, index) => {
 					return <li key={index} className='w-100 row'>
-						<b className='col-9'>{methodName}</b> <small className={`col-3 badge btn-${usedSelectors.includes(hashed) ? 'success' : 'warning'}`}>{hashed}</small>
+						<b className='col-8'>
+							{method.name}
+						</b> <small
+							className={`col-3 badge btn-${method.signatureInDiamond ? (method.functionVersionInDiamond ? 'primary' : 'success') : 'danger'}`}>
+							<abbr title={method.signatureInDiamond ? (method.functionVersionInDiamond ? 'This version is connected to the diamond ' : 'This function exists within the diamond') : 'Not in diamond!'}>
+								{method.hashed}
+							</abbr>
+						</small>
+						<input
+							type='checkbox'
+							checked={method.selected}
+							onChange={() => updateSelected(index)}
+							className='col-1 form-check-input' />
 					</li>
 				})}
 			</ul>
-			{mainDiamond && <button disabled={selectorsInDiamond !== undefined} onClick={async () => {
-				let facetInstance = new ethers.Contract(address, abi, signer);
-				let aux = [...usedSelectors];
-				const cutItem = {
-					facetAddress: address,
-					action: FacetCutAction_ADD,
-					functionSelectors: getSelectors(facetInstance, aux).filter(item => !usedSelectors.includes(item))
-				}
-				console.log(cutItem);
-				await mainDiamond.diamondCut(
-					[cutItem], ethers.constants.AddressZero, ethers.utils.toUtf8Bytes('')
-				);
-			}} className='btn btn-primary'>
-				Add Facet
-			</button>}
+			{mainDiamond && <div className='row'>
+				<button
+					disabled={queriedFacets === undefined || functions === undefined}
+					onClick={() => callItemCuts(FacetCutAction_ADD)}
+					className='btn btn-primary col-12 col-md-4'>
+						Add Selected Facets
+				</button>
+				<button
+					disabled={queriedFacets === undefined || functions === undefined || selectorsInDiamond === undefined}
+					onClick={() => callItemCuts(FacetCutAction_REPLACE)}
+					className='btn btn-warning col-12 col-md-4'>
+						Replace Selected Facets
+				</button>
+				<button
+					disabled={queriedFacets === undefined || functions === undefined || selectorsInDiamond === undefined}
+					onClick={() => callItemCuts(FacetCutAction_REMOVE)}
+					className='btn btn-danger col-12 col-md-4'>
+						Remove Selected Facets
+				</button>
+			</div>}
 		</>}
 		<br/>
-		{setMainDiamond && <button onClick={async () => {
-			await window.ethereum.request({ method: 'eth_requestAccounts' });
-			const diamondLouper = {...DiamondLoupeFacet};
-			const diamondCutData = {...DiamondCutFacet};
-
-			let instance = new ethers.Contract(address, abi, signer);
-			let aux = [...usedSelectors];
-
-			getSelectors(instance, aux);
-			instance = new ethers.Contract(address, diamondLouper.abi, signer);
-			let facets = await instance.facets();
-			let selectors = []
-			facets.forEach((item, index) => {
-				selectors = selectors.concat([...item.functionSelectors]);
-			});
-			setUsedSelectors(selectors);
-			console.log(selectors);
-			setQueriedFacets(facets);
-			getSelectors(instance, aux);
-			instance = new ethers.Contract(address, diamondCutData.abi, signer);
-			setMainDiamond(instance);
-		}} className='btn btn-primary'>
-			Connect Diamond
-		</button>}
+		{connectMainDiamondFactory &&
+			<button onClick={() => connectMainDiamondFactory(address, abi)} className='btn btn-primary'>
+				Connect Diamond
+			</button>}
 	</div>
 }
 
@@ -105,6 +138,58 @@ const App = () => {
 	const [provider, setProvider] = useState();
 	const [signer, setSigner] = useState();
 	const [combinedAbiData, setCombinedAbiData] = useState();
+	const [facetArray, setFacetArray] = useState([]);
+	const [mainFactoryDiamondContract, setMainFactoryDiamondContract] = useState();
+
+	const connectMainDiamondFactory = async (address, abi) => {
+		await window.ethereum.request({ method: 'eth_requestAccounts' });
+		const diamondCutData = {...facetArray[0]};
+		const diamondLouper = {...facetArray[1]};
+
+		let instance = new ethers.Contract(address, abi, signer);
+		let aux = [...usedSelectors];
+
+		getSelectors(instance, aux);
+		instance = new ethers.Contract(address, diamondLouper.abi, signer);
+		let facets = await instance.facets();
+		let selectors = []
+		facets.forEach((item, index) => {
+			selectors = selectors.concat([...item.functionSelectors]);
+		});
+		setUsedSelectors(selectors);
+		console.log(selectors);
+		setQueriedFacets(facets);
+		getSelectors(instance, aux);
+		instance = new ethers.Contract(address, diamondCutData.abi, signer);
+		setMainDiamond(instance);
+	}
+
+	useEffect(() => {
+		const queryContractData = async (chainId) => {
+			let data = [];
+			let first = true;
+			for await (let facetName of facetNames) {
+				let facetData = await import(`./contractAbis/${chainId}/${facetName}.json`);
+				if (first) {
+					first = false;
+					setMainFactoryDiamondContract(facetData);
+				} else {
+					data.push(facetData);
+				}
+			}
+			setFacetArray(data);
+		}
+
+		window.ethereum.on('chainChanged', (chainId) => {
+			console.log('ChainID changed!');
+			queryContractData(chainId);
+		});
+
+		window.ethereum.on('connect', async (connectionInfo) => {
+			console.log('Metamask connected!');
+			queryContractData(connectionInfo.chainId);
+		});
+	}, []);
 
 	useEffect(() => {
 		if (window.ethereum) {
@@ -114,29 +199,28 @@ const App = () => {
 		}
 	}, []);
 
-	const facetArray = [
-		DiamondCutFacet,
-		DiamondLoupeFacet,
-		ERC777ReceiverFacet,
-		OwnershipFacet,
-		creatorFacet,
-		TokensFacet,
-		ERC721Facet,
-		RAIRMetadataFacet,
-		RAIRProductFacet,
-		RAIRRangesFacet,
-	]
-
-	return <div className='container-fluid p-5'>
+	return <div className='container-fluid bg-dark text-white p-5'>
 		<div className='row h1'>
 			Rair Diamond Manager
 		</div>
 		<hr/>
 		<h5> Main Factory Diamond </h5>
-		<ContractData FacetData={FactoryDiamond} {...{signer, provider, setMainDiamond, usedSelectors, setUsedSelectors, queriedFacets, setQueriedFacets}} />
+		{mainFactoryDiamondContract && 
+			<ContractData
+				FacetData={mainFactoryDiamondContract}
+				{...{
+					connectMainDiamondFactory,
+					signer,
+					provider,
+					setMainDiamond,
+					usedSelectors,
+					setUsedSelectors,
+					queriedFacets,
+					setQueriedFacets
+				}} />}
 		<hr/>
 		<div className='row'>
-			{facetArray
+			{facetArray.length > 0 && facetArray
 				.map((contract, index) => {
 					return <ContractData key={index} FacetData={contract} {...{signer, provider, mainDiamond, usedSelectors, setUsedSelectors, queriedFacets, setQueriedFacets}} />
 				})}
@@ -144,7 +228,7 @@ const App = () => {
 		<div className='row'>
 			<button onClick={async () => {
 				let usedFunctions = [];
-				let combinedABI = [...facetArray, FactoryDiamond]
+				let combinedABI = [...facetArray, mainFactoryDiamondContract]
 					.reduce((finalList, contract) => {
 						let cleanAbi = contract.abi.filter(item => {
 							if (!usedFunctions.includes(item.name)) {
