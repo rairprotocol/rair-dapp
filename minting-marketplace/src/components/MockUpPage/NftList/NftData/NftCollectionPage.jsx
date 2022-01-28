ddababc9be792badf84d2e6784dacfa323234952
Arsenii Pilipenko <apilipenko@vp.software>
2022-01-28 15:42:40 +0200

    Added function goback and scrollTop

--- minting-marketplace/src/components/MockUpPage/NftList/NftData/NftCollectionPage.jsx
+++ minting-marketplace/src/components/MockUpPage/NftList/NftData/NftCollectionPage.jsx
@@ -4,6 +4,7 @@ import { NftItemForCollectionView } from "../NftItemForCollectionView";
 import { BreadcrumbsView } from "../Breadcrumbs/Breadcrumbs";
 import { useDispatch } from "react-redux";
 import setDocumentTitle from "../../../../utils/setTitle";
+import { useHistory } from "react-router-dom";
 
 const NftCollectionPageComponent = ({
   blockchain,
@@ -23,6 +24,7 @@ const NftCollectionPageComponent = ({
   offerPrice,
   data,
 }) => {
+  const history = useHistory();
   const dispatch = useDispatch();
   useEffect(() => {
     setDocumentTitle("Collection");
@@ -30,10 +32,32 @@ const NftCollectionPageComponent = ({
       type: "SHOW_SIDEBAR_TRUE",
     });
   }, [dispatch]);
+
+  useEffect(() => {
+    window.scroll(0, 0)
+  }, [])
+
+  const goBack = () => {
+    history.goBack();
+  }
+
+
   const defaultImg =
     "https://rair.mypinata.cloud/ipfs/QmNtfjBAPYEFxXiHmY5kcPh9huzkwquHBcn9ZJHGe7hfaW";
+
+
+
   if (tokenData.length === 0) {
-    return <h1>Dont have product</h1>;
+    return <>
+      <div style={{
+        cursor: "pointer",
+        color: "rgb(232, 130, 213)",
+        fontSize: "2rem"
+      }} onClick={() => goBack()} className="arrow-back">
+        <i class="fas fa-arrow-alt-circle-left"></i>
+      </div>
+      <h1>Don't have product</h1>
+    </>
   }
   return (
     <div
@@ -50,48 +74,48 @@ const NftCollectionPageComponent = ({
       <div className={"list-button-wrapper"}>
         {tokenData.length > 0
           ? tokenData.map((token, index) => {
-              if (token.cover !== "none") {
-                return (
-                  <NftItemForCollectionView
-                    key={`${token.id + "-" + token.productId + index}`}
-                    pict={token.cover ? token.cover : defaultImg}
-                    metadata={token.metadata}
-                    contract={token.contract}
-                    token={token.token}
-                    handleClickToken={handleClickToken}
-                    setSelectedToken={setSelectedToken}
-                    selectedToken={selectedToken}
-                    offerPrice={offerPrice}
-                    ownerAddress={token.ownerAddress}
-                    blockchain={blockchain}
-                    currentUser={currentUser}
-                    offerData={offerData}
-                    primaryColor={primaryColor}
-                    productsFromOffer={productsFromOffer}
-                    selectedData={selectedData}
-                    textColor={textColor}
-                    tokenData={tokenData}
-                    totalCount={totalCount}
-                    product={product}
-                    index={index}
-                  />
-                );
-              } else {
-                return null;
-              }
-            })
-          : Array.from(new Array(10)).map((item, index) => {
+            if (token.cover !== "none") {
               return (
-                <Skeleton
-                  key={index}
-                  className={"skeloton-product"}
-                  variant="rectangular"
-                  width={283}
-                  height={280}
-                  style={{ borderRadius: 20 }}
+                <NftItemForCollectionView
+                  key={`${token.id + "-" + token.productId + index}`}
+                  pict={token.cover ? token.cover : defaultImg}
+                  metadata={token.metadata}
+                  contract={token.contract}
+                  token={token.token}
+                  handleClickToken={handleClickToken}
+                  setSelectedToken={setSelectedToken}
+                  selectedToken={selectedToken}
+                  offerPrice={offerPrice}
+                  ownerAddress={token.ownerAddress}
+                  blockchain={blockchain}
+                  currentUser={currentUser}
+                  offerData={offerData}
+                  primaryColor={primaryColor}
+                  productsFromOffer={productsFromOffer}
+                  selectedData={selectedData}
+                  textColor={textColor}
+                  tokenData={tokenData}
+                  totalCount={totalCount}
+                  product={product}
+                  index={index}
                 />
               );
-            })}
+            } else {
+              return null;
+            }
+          })
+          : Array.from(new Array(10)).map((item, index) => {
+            return (
+              <Skeleton
+                key={index}
+                className={"skeloton-product"}
+                variant="rectangular"
+                width={283}
+                height={280}
+                style={{ borderRadius: 20 }}
+              />
+            );
+          })}
       </div>
       <div>
         <h1>Show More</h1>
