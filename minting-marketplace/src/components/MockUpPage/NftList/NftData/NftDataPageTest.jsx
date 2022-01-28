9b96c674725ebaeeb57a16cc5398971a62209f03
Arsenii Pilipenko <apilipenko@vp.software>
2022-01-28 17:06:00 +0200

    Refactoring AuthenticityBlock

--- minting-marketplace/src/components/MockUpPage/NftList/NftData/NftDataPageTest.jsx
+++ minting-marketplace/src/components/MockUpPage/NftList/NftData/NftDataPageTest.jsx
@@ -22,6 +22,7 @@ import chainData from '../../../../utils/blockchainData.js';
 
 import "react-multi-carousel/lib/styles.css";
 import { BreadcrumbsView } from "../Breadcrumbs/Breadcrumbs.jsx";
+import AuthenticityBlock from "./AuthenticityBlock/AuthenticityBlock.jsx";
 
 const NftDataPageTest = ({
   blockchain,
@@ -1011,45 +1012,10 @@ const NftDataPageTest = ({
             <></>
           )} */}
         </div>
-        <div className="block-authenticity">
-          <div className="authenticity-title">Authenticity</div>
-          <div className="table-authenticity">
-            <div className="table-authenticity-title">Action</div>
-            <div className="authenticity-box">
-              <div className="link-block">
-                <span><i class="fas fa-external-link-alt"></i></span>
-                {
-                  tokenData.map((el, index) => {
-                    if (Number(el.token) === Number(selectedToken)) {
-                      return (
-                        <a
-                          className="nftDataPageTest-a-hover"
-                          key={index}
-                          href={el?.authenticityLink}
-                          target="_blank"
-                        >
-                          Etherscan transaction
-                        </a>
-                      );
-                    }
-                  })
-                }
-              </div>
-              <div className="block-arrow">
-                <i class="fas fa-arrow-right"></i>
-              </div>
-            </div>
-            <div className="authenticity-box">
-              <div className="link-block">
-                <span><i class="fas fa-external-link-alt"></i></span>
-                <p>View on IPFS</p>
-              </div>
-              <div className="block-arrow">
-                <i class="fas fa-arrow-right"></i>
-              </div>
-            </div>
-          </div>
-        </div>
+        <AuthenticityBlock
+          tokenData={tokenData}
+          selectedToken={selectedToken}
+        />
       </div>
     </div>
   );
