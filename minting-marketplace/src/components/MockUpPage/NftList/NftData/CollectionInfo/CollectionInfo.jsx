ad4ddc0a2f24d07ecc424a2546e2620391e3cc48
Arsenii Pilipenko <apilipenko@vp.software>
2022-01-31 17:30:59 +0200

    Creating CollectionInfo Component

---
+++ minting-marketplace/src/components/MockUpPage/NftList/NftData/CollectionInfo/CollectionInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CollectionInfo = () => {
+    return <div className="contianer-collection-info">
+        <div className="collection-info-title">
+            123
+        </div>
+    </div>;
+};
+
+export default CollectionInfo;
