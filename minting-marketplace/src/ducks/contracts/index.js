f215a788979b6bce7d34eab1273ce08c224887a4
Juan Miguel Sanchez Mola <jmsm412@gmail.com>
2022-02-03 15:46:53 -0400

    Disable All Diamond UIs with an environment variable

--- minting-marketplace/src/ducks/contracts/index.js
+++ minting-marketplace/src/ducks/contracts/index.js
@@ -16,15 +16,15 @@ const contractAddresses = {
 		factory: '0xF9DC1787179c1404910adE194A5087cEbD5c4717',
 		erc777: '0xc76c3ebEA0aC6aC78d9c0b324f72CA59da36B9df',
 		minterMarketplace: '0x14ef15A945b6Cae28f4FA3862E41d74E484Cd3B5',
-		diamondFactory: '0xEF85370b8F136E2F28eA904bF0dA5acac3D1d74f',
-		diamondMarketplace: '0x6B3c06b39Aa1ADe73c625b184326d4837c7a2b64'
+		diamondFactory: process.env.REACT_APP_DIAMONDS_ENABLED === 'true' && '0xEF85370b8F136E2F28eA904bF0dA5acac3D1d74f',
+		diamondMarketplace: process.env.REACT_APP_DIAMONDS_ENABLED === 'true' && '0x6B3c06b39Aa1ADe73c625b184326d4837c7a2b64'
 	},
 	'0x13881': { // Matic Mumbai
 		factory: '0xB8083810Fa33e7Ebd777c8cd6eBB453948aFd354',
 		erc777: '0x0Ce668D271b8016a785Bf146e58739F432300B12',
 		minterMarketplace: '0x4594D508cDa05D016571082d467889f4629e1f56',
-		diamondFactory: '0xbB236Ce48dDCb58adB8665E220FE976bA5d080a5',
-		diamondMarketplace: '0x2c8BA9f098CD319a971cE2705F26723c81044Cb0'
+		diamondFactory: process.env.REACT_APP_DIAMONDS_ENABLED === 'true' && '0xbB236Ce48dDCb58adB8665E220FE976bA5d080a5',
+		diamondMarketplace: process.env.REACT_APP_DIAMONDS_ENABLED === 'true' && '0x2c8BA9f098CD319a971cE2705F26723c81044Cb0'
 	},
 	'0x89': {
 		factory: '0x9bB24e68d952Ab872b0421297E0cda8D89B2390B',
@@ -55,6 +55,7 @@ const InitialState = {
 };
 
 export default function userStore(state = InitialState, action) {
+	console.log(process.env);
 	switch (action.type) {
 		case types.SET_CHAIN_ID:
 			if (contractAddresses[action.payload] !== undefined) {
