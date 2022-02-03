90ae5949ae1af849786d7feb53e782b1587146d1
Chris Rose <chris@rair.tech>
2022-02-03 01:16:35 +0000

    Revert "revert environment variable for mainnet polygon. (pull request #324)"

--- rairnode/bin/integrations/ethers/tokenValidation.js
+++ rairnode/bin/integrations/ethers/tokenValidation.js
@@ -3,7 +3,7 @@ const RAIR_ERC721Abi = require('./contracts/RAIR_ERC721.json').abi;
 
 const endpoints = {
 	'0x13881': process.env.MATIC_TESTNET_RPC,
-	'polygon': process.env.MATIC_MAINNET_RPC,
+	'0x89': process.env.MATIC_MAINNET_RPC,
 	'0x61': process.env.BINANCE_TESTNET_RPC,
 	'0x1': process.env.ETHEREUM_MAINNET_RPC,
 	'0x5': process.env.ETHEREUM_TESTNET_GOERLI_RPC
