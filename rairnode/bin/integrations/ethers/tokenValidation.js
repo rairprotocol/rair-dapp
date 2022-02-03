dea02b6744c47a5e07399657b5268d38d42780e8
Juan Miguel Sanchez Mola <jmsm412@gmail.com>
2022-02-02 21:07:51 -0400

    Add names as valid blockchain identifiers

--- rairnode/bin/integrations/ethers/tokenValidation.js
+++ rairnode/bin/integrations/ethers/tokenValidation.js
@@ -3,10 +3,15 @@ const RAIR_ERC721Abi = require('./contracts/RAIR_ERC721.json').abi;
 
 const endpoints = {
 	'0x13881': process.env.MATIC_TESTNET_RPC,
+	'mumbai': process.env.MATIC_TESTNET_RPC,
 	'0x89': process.env.MATIC_MAINNET_RPC,
+	'matic': process.env.MATIC_MAINNET_RPC,
 	'0x61': process.env.BINANCE_TESTNET_RPC,
+	'binance-testnet': process.env.BINANCE_TESTNET_RPC,
 	'0x1': process.env.ETHEREUM_MAINNET_RPC,
+	'ethereum': process.env.ETHEREUM_MAINNET_RPC,
 	'0x5': process.env.ETHEREUM_TESTNET_GOERLI_RPC
+	'goerli': process.env.ETHEREUM_TESTNET_GOERLI_RPC
 }
 
 /**
@@ -42,6 +47,6 @@ async function checkBalanceSingle (accountAddress, blockchain, contractAddress, 
 }
 
 module.exports = {
-  checkBalanceProduct,
-  checkBalanceSingle
+	checkBalanceProduct,
+	checkBalanceSingle
 }
