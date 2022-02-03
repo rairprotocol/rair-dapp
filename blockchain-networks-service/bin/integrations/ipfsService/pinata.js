1b7603d22dea54e861373b6d9576cb05d926195a
valeriy-kovalev <valeriykovalyov.mail@gmail.com>
2022-02-03 22:16:54 +0200

    add additional function to native ipfs provider; refactoring;

--- blockchain-networks-service/bin/integrations/ipfsService/pinata.js
+++ blockchain-networks-service/bin/integrations/ipfsService/pinata.js
@@ -7,64 +7,75 @@ const log = require('../../utils/logger')(module);
 const retrieveMediaInfo = (CID) => axios.get(`${ process.env.PINATA_GATEWAY }/${ CID }/rair.json`);
 
 const addPin = async (CID, name, socketInstance) => {
-  try {
-    const response = await pinata.pinByHash(CID, {
-      pinataMetadata: {
-        name: `RAIR_${ name }`
-      }
-    });
+    try {
+        const response = await pinata.pinByHash(CID, {
+            pinataMetadata: {
+                name: `RAIR_${ name }`
+            }
+        });
 
-    log.info(`Pinned to PINATA: ${ JSON.stringify(response) }`);
+        log.info(`Pinned to PINATA: ${ JSON.stringify(response) }`);
 
-    if (!_.isUndefined(socketInstance)) socketInstance.emit('uploadProgress', {
-      message: 'Pined to Pinata.',
-      last: true,
-      done: 100
-    });
-  } catch (err) {
-    log.error(`Pinning to PINATA: ${ err.message }`);
-  }
+        if (!_.isUndefined(socketInstance)) socketInstance.emit('uploadProgress', {
+            message: 'Pined to Pinata.',
+            last: true,
+            done: 100
+        });
+    } catch (err) {
+        log.error(`Pinning to PINATA: ${ err.message }`);
+    }
 };
 
 const removePin = async (CID) => {
-  try {
-    const response = await pinata.unpin(CID);
+    try {
+        const response = await pinata.unpin(CID);
 
-    log.info(`Unpin PINATA: ${ CID }, status: ${ response }`);
+        log.info(`Unpin PINATA: ${ CID }, status: ${ response }`);
 
-    return response;
-  } catch (err) {
-    log.error(`Could not remove pin from PINATA ${ CID }: ${ err }`);
-    return {};
-  }
+        return response;
+    } catch (err) {
+        log.error(`Could not remove pin from PINATA ${ CID }: ${ err }`);
+        return {};
+    }
 };
 
 const addFolder = async (pathTo, folderName, socketInstance) => {
-  const response = await pinata.pinFromFS(pathTo, {
-    pinataMetadata: {
-      name: folderName
-    }
-  });
+    const response = await pinata.pinFromFS(pathTo, {
+        pinataMetadata: {
+            name: folderName
+        }
+    });
 
-  socketInstance.emit('uploadProgress', { message: `added files to Pinata`, last: false, part: false });
+    socketInstance.emit('uploadProgress', { message: `added files to Pinata`, last: false, part: false });
 
-  return _.get(response, 'IpfsHash');
+    return _.get(response, 'IpfsHash');
 };
 
 const addMetadata = async (data, name) => {
-  const response = await pinata.pinJSONToIPFS(data, {
-    pinataMetadata: {
-      name
-    }
-  });
+    const response = await pinata.pinJSONToIPFS(data, {
+        pinataMetadata: {
+            name
+        }
+    });
 
-  return _.get(response, 'IpfsHash');
-}
+    return _.get(response, 'IpfsHash');
+};
+
+const addFile = async (pathTo, name) => {
+    const response = await pinata.pinFromFS(pathTo, {
+        pinataMetadata: {
+            name
+        }
+    });
+
+    return _.get(response, 'IpfsHash');
+};
 
 module.exports = {
-  retrieveMediaInfo,
-  removePin,
-  addPin,
-  addFolder,
-  addMetadata
+    retrieveMediaInfo,
+    removePin,
+    addPin,
+    addFolder,
+    addMetadata,
+    addFile
 };
