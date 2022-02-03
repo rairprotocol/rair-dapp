51b075fa57f294d2b10323569c137fee8d381af1
Arsenii Pilipenko <apilipenko@vp.software>
2022-02-03 17:21:00 +0200

    CollectionInfo Block with using real data

--- minting-marketplace/src/components/MockUpPage/NftList/NftData/CollectionInfo/CollectionInfo.jsx
+++ minting-marketplace/src/components/MockUpPage/NftList/NftData/CollectionInfo/CollectionInfo.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./CollectionInfo.css";
 import chainDataFront from "./../../../utils/blockchainDataFront";
 
-const CollectionInfo = ({ defaultImg, blockchain }) => {
+const CollectionInfo = ({ defaultImg, blockchain, offerData }) => {
+
+    console.log(offerData, "offerData");
+
     return <div className="wrapper-collection-info">
         {/* <div className="collection-info-head">
             Collection info
@@ -23,23 +26,30 @@ const CollectionInfo = ({ defaultImg, blockchain }) => {
                 </div>
             </div>
             <div className="collection-info-body">
-                <div className="block-item-collection">
-                    <div className="item-name">
-                        <img src={defaultImg} alt="rair-tech" />
-                        <div className="item-name-text">
-                            #01 (UR)
+                {
+                   offerData && offerData?.map((token, index) => {
+                        return <div key={index + token.price} className="block-item-collection">
+                            <div className="item-name">
+                                <img src={defaultImg} alt="rair-tech" />
+                                <div className="item-name-text">
+                                    {token.offerName}
+                                </div>
+                            </div>
+                            <div className="item-rank">
+                                {token.offerIndex === 0 && "Ultra Rair"}
+                                {token.offerIndex === 1 && "Rair"}
+                                {token.offerIndex >= 2 && "Common"}
+                            </div>
+                            <div className="item-availa">
+                                <p>{token.soldCopies} / {token.copies}</p>
+                            </div>
+                            <div className="item-price">
+                                <img src={chainDataFront[blockchain]?.image} alt="blockchain" />
+                                {token.price}
+                            </div>
                         </div>
-                    </div>
-                    <div className="item-rank">
-                        <p>Ultra Rair</p>
-                    </div>
-                    <div className="item-availa">
-                        <p>1 /1 </p>
-                    </div>
-                    <div className="item-price">
-                        <img src={chainDataFront[blockchain]?.image} alt="blockchain" />
-                    </div>
-                </div>
+                    })
+                }
             </div>
         </div>
     </div>
