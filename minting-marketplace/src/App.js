97bafeacb56c1173686c4f9088ed953dfe425ffd
Juan Miguel Sanchez Mola <jmsm412@gmail.com>
2022-02-07 12:56:42 -0400

    Fix String

--- minting-marketplace/src/App.js
+++ minting-marketplace/src/App.js
@@ -244,7 +244,7 @@ function App({ sentryHistory }) {
 	}, [dispatch])
 
 	useEffect(() => {
-		setTitle('Welcome);
+		setTitle('Welcome');
 		if (process.env.NODE_ENV === 'development') {
 			window.gotoRouteBackdoor = sentryHistory.push
 			window.adminAccessBackdoor = (boolean) => {
