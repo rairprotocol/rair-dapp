5ecb32eb2d4929074a2a0eeb0aea9ac41e9cfd7b
Arsenii Pilipenko <apilipenko@vp.software>
2022-02-17 15:36:29 +0200

    Added linkedin-in links

--- minting-marketplace/src/components/SplashPage/TeamMeet/TeamMeetList.jsx
+++ minting-marketplace/src/components/SplashPage/TeamMeet/TeamMeetList.jsx
@@ -170,8 +170,8 @@ const teamAboutRair = [
         socials: [
             {
                 classIcon: 'fab fa-linkedin-in',
-                link: '',
-                classLink: "arrrow-right"
+                link: 'https://www.linkedin.com/in/ed-prado-a8526a4/',
+                classLink: ""
             }
         ]
     },
@@ -192,8 +192,8 @@ const teamAboutRair = [
         socials: [
             {
                 classIcon: 'fab fa-linkedin-in',
-                link: '',
-                classLink: "arrrow-right"
+                link: 'https://www.linkedin.com/in/garrettminks/',
+                classLink: ""
             }
         ]
     },
@@ -229,6 +229,13 @@ const teamAboutRair = [
             the first tracking and logistics integration applications for an 
             international shipping company.
             `
+        ],
+        socials: [
+            {
+                classIcon: 'fab fa-linkedin-in',
+                link: 'https://www.linkedin.com/in/martin-casado-484b353/',
+                classLink: ""
+            }
         ]
     }
 ];
@@ -261,8 +268,8 @@ const rairAdvisorsTeam = [
         socials: [
             {
                 classIcon: 'fab fa-linkedin-in',
-                link: '',
-                classLink: "arrrow-right"
+                link: 'https://www.linkedin.com/in/michaelterpin/',
+                classLink: ""
             }
         ]
     },
@@ -301,8 +308,8 @@ const rairAdvisorsTeam = [
         socials: [
             {
                 classIcon: 'fab fa-linkedin-in',
-                link: '',
-                classLink: "arrrow-right"
+                link: 'https://www.linkedin.com/in/davidnjensen/',
+                classLink: ""
             }
         ]
     },
@@ -384,8 +391,8 @@ const rairAdvisorsTeam = [
         socials: [
             {
                 classIcon: 'fab fa-linkedin-in',
-                link: '',
-                classLink: "arrrow-right"
+                link: 'https://www.linkedin.com/in/mattneco/',
+                classLink: ""
             }
         ]
     }
