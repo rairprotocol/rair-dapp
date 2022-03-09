31edd3cd7545867c1af3c3858ddd3c11ca6ea97f
Brian Fogg <fogg4444@gmail.com>
2022-03-09 12:29:01 -0800

    removing extra return exit code

--- rairnode/bin/index.js
+++ rairnode/bin/index.js
@@ -1,7 +1,7 @@
 const port = process.env.PORT;
 
+// initialize app role token manager
 const { vaultAppRoleTokenManager } = require('./vault/vaultAppRoleTokenManager');
-return
 
 const express = require('express');
 const bodyParser = require('body-parser');
