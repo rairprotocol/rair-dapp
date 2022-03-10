83ac2e82ec3b0fae53f90de55b56dc0ba3af8c18
Brian Fogg <fogg4444@gmail.com>
2022-03-10 10:21:44 -0800

    cleanup

--- terraform/modules/vault_config/kv_primary_test.tf
+++ terraform/modules/vault_config/kv_primary_test.tf
@@ -1,28 +1,5 @@
-resource "vault_mount" "primary" {
-  path        = "primary"
-  type        = "kv-v2"
-  description = "Primary KV secret storage"
-}
-
 resource "vault_mount" "key_storage" {
   path        = "key_storage"
   type        = "kv-v2"
   description = "Key storage"
-}
-
-locals {
-  generic_secret_test_sub_path = "test"
-}
-
-resource "vault_generic_secret" "test" {
-  depends_on = [
-    vault_mount.primary
-  ]
-  path = "${vault_mount.primary.path}/${local.generic_secret_test_sub_path}"
-
-  data_json = <<EOT
-{
-  "initial-secret": "changeme"
-}
-EOT
 }
--- terraform/modules/vault_config/policies.tf
+++ terraform/modules/vault_config/policies.tf
@@ -1,13 +1,3 @@
-resource "vault_policy" "primary" {
-  name = "primary"
-
-  policy = <<EOT
-path "${vault_mount.primary.path}/data/${local.generic_secret_test_sub_path}" { 
-  capabilities = ["create", "read", "update", "delete", "list"]
-}
-EOT
-}
-
 resource "vault_policy" "key_storage_admin" {
   name = "key-storage-admin"
 
