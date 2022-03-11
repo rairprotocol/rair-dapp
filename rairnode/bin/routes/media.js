5d23bdcebed34d5e4e86b3cadc920e1de2244bf3
Brian Fogg <fogg4444@gmail.com>
2022-03-11 08:59:41 -0800

    plugging in vault writer code

--- rairnode/bin/routes/media.js
+++ rairnode/bin/routes/media.js
@@ -12,6 +12,8 @@ const log = require('../utils/logger')(module);
 //const { execPromise } = require('../utils/helpers');
 const { checkBalanceSingle } = require('../integrations/ethers/tokenValidation.js');
 const { generateThumbnails, getMediaData, convertToHLS, encryptFolderContents } = require('../utils/ffmpegUtils.js');
+const { vaultKeyWriter } = require('../vault/vaultKeyWriter');
+const { vaultAppRoleTokenManager } = require('../vault/vaultAppRoleTokenManager');
 
 module.exports = context => {
   const router = express.Router();
@@ -325,13 +327,28 @@ module.exports = context => {
           last: false
         });
 
+        const key = exportedKey.toJSON();
+
         await db.File.create({
           _id: cid,
-          key: exportedKey.toJSON(),
+          key,
           uri: storageLink,
           ...meta,
         });
-        // TODO: add vault key writer here
+
+        try {
+          const vaultWriteRes = await vaultKeyWriter.write({
+            secretName: cid,
+            data: {
+              uri: storageLink,
+              key
+            },
+            vaultToken: vaultAppRoleTokenManager.getToken()
+          })
+          console.log('ress', vaultWriteRes);
+        } catch(err) {
+          console.log('Error writing key to vault:', cid)
+        }
 
         log.info(`${ req.file.originalname } stored to DB.`);
         socketInstance.emit('uploadProgress', { message: 'Stored to database.', last: ['gcp'].includes(storage) ? true : false, done: ['gcp'].includes(storage) ? 100 : 96 });
