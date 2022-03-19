f83d857843ded9046b2ce3a5982193b64dbe0df6
Brian Fogg <fogg4444@gmail.com>
2022-03-18 17:24:09 -0700

    modifying name of resrouces

--- terraform/environments/cloudflare/main.tf
+++ terraform/environments/cloudflare/main.tf
@@ -17,10 +17,10 @@ terraform {
 provider "cloudflare" {
 }
 
-resource "cloudflare_zone" "rair-tech" {
-    zone = "rair.tech"
+resource "cloudflare_zone" "rair_tech" {
+  zone = "rair.tech"
 }
 
-resource "cloudflare_zone" "rair-market" {
-    zone = "rair.market"
+resource "cloudflare_zone" "rair_market" {
+  zone = "rair.market"
 }
