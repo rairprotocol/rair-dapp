1e520f11c2efe7aa873adee3b9884a0af1036d7a
mda100 <mdadams10@gmail.com>
2022-03-21 18:48:44 -0700

    unit testing

--- minting-marketplace/src/components/SplashPage/SplashPageTemplate/VideoPlayer/VideoPlayerBySignature .jsx
+++ minting-marketplace/src/components/SplashPage/SplashPageTemplate/VideoPlayer/VideoPlayerBySignature .jsx
@@ -3,88 +3,84 @@ import { useSelector } from "react-redux";
 import Swal from "sweetalert2";
 import setDocumentTitle from "../../../../utils/setTitle";
 
+const VideoWindow = ({mediaAddress}) => {
+  return (
+    <div className="col-12 row mx-0 bg-secondary h1">
+    <video
+      // id={"vjs-" + videoName}
+      // className="video-js vjs-16-9"
+      controls
+      preload="auto"
+      data-setup="{}"
+    >
+      <source
+        // autostart="false"
+        src={mediaAddress}
+        type="video/mp4"
+      />
+    </video>
+  </div>
+  )
+}
+
+const VideoWindowError = () => {
+  return (
+    <div className="col-12 row mx-0 bg-secondary h1">
+      Metamask Signature Required to Access Video
+    </div>
+  )
+}
+
 const VideoPlayerBySignature = ({mediaAddress}) => {
   const { programmaticProvider } = useSelector((state) => state.contractStore);
   const {signature, setSignature} = useState(null)
-  const [videoName] = useState(Math.round(Math.random() * 10000));
 
   const requestChallenge = useCallback(async () => {
+
     let sign;
     let parsedResponse;
-    if (window.ethereum) {
-      let [account] = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      let response = await (
-        await fetch("/api/auth/get_challenge/" + account)
-      ).json();
-      parsedResponse = JSON.parse(response.response);
-      try {
-      sign = await window.ethereum.request({
-        method: "eth_signTypedData_v4",
-        params: [account, response.response],
-        from: account,
-      })
-      setSignature(sign);
-    }
-    catch (error){}
-    } else if (programmaticProvider) {
+
+    if (programmaticProvider) {
       let response = await (
         await fetch("/api/auth/get_challenge/" + programmaticProvider.address)
       ).json();
       parsedResponse = JSON.parse(response.response);
       let { EIP712Domain, ...revisedTypes } = parsedResponse.types;
-      try {
-        sign = await programmaticProvider._signTypedData(
-          parsedResponse.domain,
-          revisedTypes,
-          parsedResponse.message
-        );
-        setSignature(sign)
-      } catch (error) {
-        Swal.fire("Metamask Signature Required to Access Video");
-      }
+      sign = await programmaticProvider._signTypedData(
+        parsedResponse.domain,
+        revisedTypes,
+        parsedResponse.message
+      );
+      setSignature(sign);
+      return;
       
     } else {
       Swal.fire("Error", "Unable to decrypt videos", "error");
       return;
     }
-    }, [videoName, programmaticProvider]);
+    }, [programmaticProvider]);
+
 
   useEffect(() => {
     requestChallenge()
   }, [requestChallenge]);
 
-  useEffect(() => {
-    setDocumentTitle(`Streaming`);
-  }, [videoName]);
-  if (signature) {
-    return (
-      <>
-        <div className="col-12 row mx-0 bg-secondary h1">
-          <video
-            // id={"vjs-" + videoName}
-            // className="video-js vjs-16-9"
-            controls
-            preload="auto"
-            data-setup="{}"
-          >
-            <source
-              // autostart="false"
-              src={mediaAddress}
-              type="video/mp4"
-            />
-          </video>
-        </div>
-      </>
-    );
-  }
-  else {
-    <div className="col-12 row mx-0 bg-secondary h1">
-      Metamask Signature Required to Access Video
-        </div>
-  }
-  
+  useEffect(() => {console.log(signature)}, [signature])
+
+
+  return (
+    <>
+    {signature ? <VideoWindow mediaAddress={mediaAddress}/> : <VideoWindowError/> }
+    </>
+  )
 };
 
 export default VideoPlayerBySignature;
+
+
+/* stuff to put back in 
+  useEffect(() => {
+    setDocumentTitle(`Streaming`);
+  }, [videoName]);
+
+*/
