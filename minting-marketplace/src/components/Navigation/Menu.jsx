5dee0cfe3d59886cbe135ba75bb44b286a9c7382
Arsenii Pilipenko <apilipenko@vp.software>
2022-03-25 23:21:19 +0200

    refactoring and added profile for menu

--- minting-marketplace/src/components/Navigation/Menu.jsx
+++ minting-marketplace/src/components/Navigation/Menu.jsx
@@ -1,67 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
 import "./Menu.css";
-import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
 import * as colorTypes from "../../ducks/colors/types";
 import * as authTypes from "../../ducks/auth/types";
 import * as contractTypes from "../../ducks/contracts/types";
-
-const Nav = styled.nav`
-  background: ${(props) => props.primaryColor === "rhyno" ? "rgb(192, 192, 192)" : "rgb(43, 40, 41)"};
-  height: 85px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1.5rem;
-  z-index: 12;
-`;
-
-const ListItem = styled.li`
-    font-size: 18px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 10vw;
-    padding: 30px 0px;
-    width: 100%;
-    &:hover {
-        background: ${props => props.primaryColor === "rhyno" ? "rgb(211, 210, 211)" : "rgb(46, 44, 45)"};
-    }
-
-    .burger-menu-logout {
-        width: 100%;
-        cursor: pointer;
-        i {
-            margin-right: 10px;
-        }
-    }
-
-    a {
-        width: 100%;
-    }
-`;
-
-const List = styled.ul`
-        background: ${props => props.primaryColor === "rhyno" ? "rgb(201, 201, 201)" : "rgb(56, 54, 55)"};
-        overflow: ${(props) => props.click && "hidden"};
-        border-bottom-right-radius:  ${(props) => props.click ? "16px" : ""};
-        border-bottom-left-radius: ${(props) => props.click ? "16px" : ""};
-        margin-top: 5px;
-        display: flex;
-        width: 100%;
-        position: absolute;
-        top: 80px;
-        left: ${(props) => props.click ? "0" : "-100%"};
-        opacity: ${(props) => props.click ? "1" : "0"};
-        align-items: ${props => props.click && "center"};
-        padding-left: ${props => props.click && "0px"};
-        transition: all 0.5s ease;
-        flex-direction: column;
-        list-style-type: none;
-        grid-gap: 0px;
-        z-index: 1;
-`;
-
+import { OnboardingButton } from '../common/OnboardingButton';
+import { List, ListItem, ListProfileItem, Nav, ProfileButtonBack } from './NavigationItems/NavigationItems';
+import { NavLink } from 'react-router-dom';
 
 const MenuNavigation = ({
     headerLogo,
@@ -71,13 +16,20 @@ const MenuNavigation = ({
     programmaticProvider,
     renderBtnConnect,
     loginDone,
-    setLoginDone
+    setLoginDone,
+    currentUserAddress
 }) => {
     const [click, setClick] = useState(false);
+    const [userData, setUserData] = useState(null)
+    const [openProfile, setOpenProfile] = useState(false);
     const dispatch = useDispatch();
 
     const toggleMenu = () => {
         setClick(prev => !prev);
+    }
+
+    const toggleOpenProfile = () => {
+        setOpenProfile(prev => !prev);
     }
 
     const logout = () => {
@@ -88,28 +40,61 @@ const MenuNavigation = ({
         toggleMenu();
     };
 
+    const getInfoFromUser = useCallback(async () => {
+        // find user
+        const result = await fetch(`/api/users/${currentUserAddress}`).then(
+            (blob) => blob.json()
+        );
+
+        if(result.success) {
+            setUserData(result.user);
+        }
+
+    }, [currentUserAddress]);
+
+    useEffect(() => {
+        getInfoFromUser();
+    }, [getInfoFromUser]);
+
     return (
         <div className="col-1 rounded burder-menu">
             <Nav primaryColor={primaryColor}>
-                <div>
-                    <img style={{ width: "50px", height: "auto" }} src={headerLogo} alt="" />
+                <div className="burder-menu-logo">
+                    <NavLink to="/">
+                        <img src={headerLogo} alt="logo_rair" />
+                    </NavLink>
                 </div>
-                <List primaryColor={primaryColor} click={click}>
+                {openProfile ? <List primaryColor={primaryColor} click={click}>
+                    <ListProfileItem>
+                        <ProfileButtonBack onClick={toggleOpenProfile}><i className="fas fa-chevron-left"></i></ProfileButtonBack>
+                        {userData && <div className="burger-menu-profile">
+                            {userData.avatar && <div><img className="burger-menu-avatar" src={userData.avatar} alt="avatar" /></div>}
+                            <div style={{margin: "10px 0"}}>Name: {userData.nickName && userData.nickName.substr(0, 20) + "..."}</div>
+                            <div>Email: {userData.email}</div>
+                        </div>}
+                    </ListProfileItem>
+                </List> : <List primaryColor={primaryColor} click={click}>
                     {!loginDone && !renderBtnConnect && <ListItem primaryColor={primaryColor}>
                         <div className='btn-connect-wallet-wrapper'>
                             <button disabled={!window.ethereum && !programmaticProvider && !startedLogin}
                                 className={`btn btn-${primaryColor} btn-connect-wallet`}
                                 onClick={connectUserData}>
                                 {startedLogin ? 'Please wait...' : 'Connect Wallet'}
-                                {/* <img alt='Metamask Logo' src={MetamaskLogo}/> */}
                             </button>
-                            {/* {renderBtnConnect ?
-                                <OnboardingButton />
-                                :
-                                <></>
-                            } */}
                         </div>
                     </ListItem>}
+                    {
+                        renderBtnConnect && <ListItem>
+                            <OnboardingButton className="borading-btn-mobile" />
+                        </ListItem>
+                    }
+                    {
+                        loginDone && <ListItem primaryColor={primaryColor}>
+                            <div className="burder-menu-profile" onClick={toggleOpenProfile}>
+                                <i className="fas fa-cog"></i>Profile settings
+                            </div>
+                        </ListItem>
+                    }
                     <ListItem primaryColor={primaryColor}>
                         <a
                             href="https://rair.tech/"
@@ -149,17 +134,12 @@ const MenuNavigation = ({
                             <i className="fas fa-sign-out-alt"></i>Logout
                         </div>
                     </ListItem>}
-                </List>
-                <ul className={click ? "nav-options active" : "nav-options"}>
-
-                </ul>
-                <div className="mobile-menu" onClick={toggleMenu}>
-                    {click ? (
-                        <i className="fa fa-times" aria-hidden="true"></i>
-                    ) : (
-                        <i className="fa fa-bars" aria-hidden="true"></i>
-                    )}
-                </div>
+                </List>}
+                {click ? <div className="mobile-menu" onClick={toggleMenu}>
+                    <i className="fa fa-times" aria-hidden="true"></i>
+                </div> : <div className="mobile-menu" onClick={() => { toggleMenu(); setOpenProfile(false) }}>
+                    <i className="fa fa-bars" aria-hidden="true"></i>
+                </div>}
             </Nav>
         </div>
     )
---
+++ minting-marketplace/src/components/Navigation/NavigationItems/NavigationItems.jsx
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+background: ${(props) => props.primaryColor === "rhyno" ? "rgb(192, 192, 192)" : "rgb(43, 40, 41)"};
+height: 85px;
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 1.5rem;
+z-index: 12;
+`;
+
+export const ListItem = styled.li`
+  font-size: 18px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 10vw;
+  padding: 30px 0px;
+  width: 100%;
+  &:hover {
+      background: ${props => props.primaryColor === "rhyno" ? "rgb(211, 210, 211)" : "rgb(46, 44, 45)"};
+  }
+
+  .burger-menu-logout, .burder-menu-profile {
+      width: 100%;
+      cursor: pointer;
+      i {
+          margin-right: 10px;
+      }
+  }
+
+  a {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+  }
+`;
+
+export const List = styled.ul`
+      background: ${props => props.primaryColor === "rhyno" ? "rgb(201, 201, 201)" : "rgb(56, 54, 55)"};
+      overflow: ${(props) => props.click && "hidden"};
+      border-bottom-right-radius:  16px;
+      border-bottom-left-radius: 16px;
+      margin-top: 5px;
+      display: flex;
+      width: 100%;
+      position: absolute;
+      top: 80px;
+      left: ${(props) => props.click ? "0" : "-100%"};
+      opacity: ${(props) => props.click ? "1" : "0"};
+      align-items: ${props => props.click && "center"};
+      padding-left: ${props => props.click && "0px"};
+      transition: all 0.5s ease;
+      flex-direction: column;
+      list-style-type: none;
+      grid-gap: 0px;
+      z-index: 1;
+`;
+
+export const ListProfileItem = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  min-height: 20vh;
+`;
+
+export const ProfileButtonBack = styled.div`
+  text-align: left;
+  cursor: pointer;
+`;
