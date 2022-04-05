51d4ee71d78ee60fc99cb0e550ce1acd7a5c9e71
Brian Fogg <fogg4444@gmail.com>
2022-04-04 21:58:41 -0700

    adding persistent storage claim

--- terraform/modules/kubernetes_infra/jenkins.tf
+++ terraform/modules/kubernetes_infra/jenkins.tf
@@ -1,45 +1,27 @@
 locals {
   jenkins_namespace = "jenkins-primary"
   jenkins_image = "jenkins/jenkins:latest"
-  # jenkins_node_port = 32000
+  jenkins_persistent_storage_name = "${local.jenkins_namespace}-persistent-storage"
+  jenkins_persistent_volume_claim_name = "${local.jenkins_namespace}-claim"
+  jenkins_default_port = 8080
 }
 
-# resource "kubernetes_namespace" "jenkins_namespace" {
-#   count = var.create_namespace ? 1 : 0
-#   metadata {
-#     annotations = {
-#       name = "jenkins"
-#     }
-
-#     labels = {
-#       managedby = "terraform"
-#     }
-
-#     name = var.namespace
-#   }
-# }
-
-# resource "kubernetes_persistent_volume_claim" "claim" {
-#   metadata {
-#     name      = "${var.name}-claim"
-#     namespace = var.namespace
-#     labels = {
-#       managedby = "terraform"
-#     }
-#   }
-#   spec {
-#     access_modes = [var.accessmode]
-#     resources {
-#       requests = {
-#         storage = var.request_storage
-#       }
-#     }
-#     storage_class_name = var.storageclass
-#   }
-#   depends_on = [
-#     kubernetes_namespace.jenkins_namespace
-#   ]
-# }
+resource "kubernetes_persistent_volume_claim" "claim" {
+  metadata {
+    name      = local.jenkins_persistent_volume_claim_name
+    labels = {
+      managedby = "terraform"
+    }
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "10Gi"
+      }
+    }
+  }
+}
 
 data "google_compute_address" "jenkins_internal_load_balancer" {
   name = var.jenkins_internal_load_balancer_name
@@ -48,16 +30,11 @@ data "google_compute_address" "jenkins_internal_load_balancer" {
 }
 
 resource "kubernetes_deployment" "jenkins" {
-  # depends_on = [
-  #   kubernetes_namespace.jenkins_namespace
-  # ]
-
   metadata {
     name = "${local.jenkins_namespace}-deployment"
     labels = {
       managedby = "terraform"
     }
-    # namespace = local.jenkins_namespace
   }
 
   spec {
@@ -81,23 +58,19 @@ resource "kubernetes_deployment" "jenkins" {
           image = local.jenkins_image
           name  = local.jenkins_namespace
           port {
-            container_port = "8080"
+            container_port = "${local.jenkins_default_port}"
           }
-          # volume_mount {
-          #   name       = "${var.name}-persistent-storage"
-          #   mount_path = "/var/jenkins_home"
-          # }
-          #   TODO: liveness probe
+          volume_mount {
+            name       = local.jenkins_persistent_storage_name
+            mount_path = "/var/jenkins_home"
+          }
         }
-        # security_context {
-        #   fs_group = "1000"
-        # }
-        # volume {
-        #   name = "${var.name}-persistent-storage"
-        #   persistent_volume_claim {
-        #     claim_name = "${var.name}-claim"
-        #   }
-        # }
+        volume {
+          name = local.jenkins_persistent_storage_name
+          persistent_volume_claim {
+            claim_name = local.jenkins_persistent_volume_claim_name
+          }
+        }
       }
     }
   }
@@ -121,33 +94,9 @@ resource "kubernetes_service" "jenkins_service" {
     }
     port {
       port        = 80
-      target_port = 8080
-      # node_port = local.jenkins_node_port
+      target_port = local.jenkins_default_port
       name = "http"
     }
     type = "LoadBalancer"
   }
 }
-
-# resource "kubernetes_service_account" "thia" {
-#   metadata {
-#     name      = "${var.name}-admin"
-#     namespace = var.namespace
-#   }
-# }
-
-# resource "kubernetes_cluster_role_binding" "this" {
-#   metadata {
-#     name = "${var.name}-rbac"
-#   }
-#   role_ref {
-#     api_group = "rbac.authorization.k8s.io"
-#     kind      = "ClusterRole"
-#     name      = "cluster-admin"
-#   }
-#   subject {
-#     kind      = "ServiceAccount"
-#     name      = "${var.name}-admin"
-#     namespace = var.namespace
-#   }
-# }
