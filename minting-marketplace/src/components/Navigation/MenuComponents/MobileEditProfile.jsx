2a0432c9f72c14ef2f1fa8657b8d83e12a762338
Arsenii Pilipenko <apilipenko@vp.software>
2022-04-13 18:48:40 +0300

    deleted console.log

--- minting-marketplace/src/components/Navigation/MenuComponents/MobileEditProfile.jsx
+++ minting-marketplace/src/components/Navigation/MenuComponents/MobileEditProfile.jsx
@@ -19,8 +19,6 @@ const MobileEditProfile = ({ primaryColor, toggleEditMode, userData, editMode, c
         setEmail(e.target.value);
     }
 
-    console.log(loading, "loading")
-
     const photoUpload = useCallback(
         (e) => {
             e.preventDefault();
