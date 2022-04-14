d0ae175f6db446d02723b649d2bc45cb0d8d6942
Brian Fogg <fogg4444@gmail.com>
2022-04-14 12:05:48 -0700

    fix

--- terraform/modules/mongo/roles.tf
+++ terraform/modules/mongo/roles.tf
@@ -1,8 +1,8 @@
 resource "mongodbatlas_custom_db_role" "read_write_primary_db" {
   project_id   = var.project_id
-  name = "read-write-primary-db"
+  role_name = "read-write-primary-db"
 
-  inheritedRoles {
+  inherited_roles {
     role_name = module.mongo_shared.built_in_roles_map.readWrite
     database_name = var.primary_db_name
   }
