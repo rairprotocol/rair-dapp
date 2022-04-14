c087154b14e5350027ba0655182885630f12fb01
Brian Fogg <fogg4444@gmail.com>
2022-04-14 11:18:51 -0700

    revising custom role

--- terraform/environments/dev/mongo_db/users.tf
+++ terraform/environments/dev/mongo_db/users.tf
@@ -3,6 +3,6 @@ resource "mongodbatlas_database_user" "rairnode" {
   project_id   = module.mongo_shared.mongo_project_id_map.dev.project_id
 
   roles {
-    role_name = mongodbatlas_custom_db_role.read_write_all_collections.name
+    role_name = mongodbatlas_custom_db_role.read_write_primary_db.name
   }
 }
