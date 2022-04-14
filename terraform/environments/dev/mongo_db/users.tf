bd66daa5121737dbe5c9758c47ac597227e585f6
Brian Fogg <fogg4444@gmail.com>
2022-04-14 11:11:53 -0700

    revising users

--- terraform/environments/dev/mongo_db/users.tf
+++ terraform/environments/dev/mongo_db/users.tf
@@ -1,4 +1,8 @@
 resource "mongodbatlas_database_user" "rairnode" {
   auth_database_name = mongodbatlas_cluster.primary.name
   project_id   = module.mongo_shared.mongo_project_id_map.dev.project_id
+
+  roles {
+    role_name = mongodbatlas_custom_db_role.read_write_all_collections.name
+  }
 }
