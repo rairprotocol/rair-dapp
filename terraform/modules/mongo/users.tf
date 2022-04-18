8f7c44c4d2cab7161c0f7a7554978239d6f38c5a
Brian Fogg <brian@rair.tech>
2022-04-18 21:58:45 +0000

    Merged in brian/fix-external-db-reference-mongo-2 (pull request #675)
    revising admin db reference for db admin users on dev
    
    * revising admin db reference for db admin users on dev

--- terraform/modules/mongo/users.tf
+++ terraform/modules/mongo/users.tf
@@ -17,13 +17,13 @@ resource "mongodbatlas_database_user" "rairnode" {
 resource "mongodbatlas_database_user" "dev_team_db_users" {
   for_each = var.dev_team_db_admins
 
-  auth_database_name = local.mongo_admin_db_name
+  auth_database_name = module.mongo_shared.mongo_external_db_name
   project_id = var.project_id
   username = each.value.username
   x509_type =  module.mongo_shared.X509Type.MANAGED
 
   roles {
     role_name = module.mongo_shared.built_in_roles_map.dbAdmin
-    database_name = module.mongo_shared.mongo_external_db_name
+    database_name = local.mongo_admin_db_name
   }
 }
