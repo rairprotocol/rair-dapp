be51441173be43df4d25c093f4266a8ec97e5b0c
Brian Fogg <brian@rair.tech>
2022-04-18 22:14:20 +0000

    Merged in brian/migrate-rairnode-user-to-x509-auth (pull request #678)
    migrating rairnode user to x509 based auth
    
    * migrating rairnode user to x509 based auth
    
    * migrating rairnode to x509 based auth

--- terraform/modules/mongo/users.tf
+++ terraform/modules/mongo/users.tf
@@ -1,8 +1,9 @@
 resource "mongodbatlas_database_user" "rairnode" {
-  auth_database_name = local.mongo_admin_db_name
+  auth_database_name = module.mongo_shared.mongo_external_db_name
   project_id   = var.project_id
   username = "rairnode"
-  
+  x509_type =  module.mongo_shared.X509Type.MANAGED
+
   roles {
     role_name = mongodbatlas_custom_db_role.read_write_primary_db.role_name
     database_name = local.mongo_admin_db_name
