05faa25a49e3a04e3c7a4ec5b3fff768f1c4f88e
Brian Fogg <brian@rair.tech>
2022-04-18 22:23:07 +0000

    Merged in brian/remove-old-custom-roles (pull request #679)
    removing old role that we're not using
    
    * removing old role that we're not using

--- terraform/modules/mongo/output.tf
+++
@@ -1,3 +0,0 @@
-output "loweset_data_access_custom_role_name" {
-  value = mongodbatlas_custom_db_role.lowest_data_access_possible.role_name
-}
--- terraform/modules/mongo/roles.tf
+++ terraform/modules/mongo/roles.tf
@@ -7,13 +7,3 @@ resource "mongodbatlas_custom_db_role" "read_write_primary_db" {
     database_name = var.primary_db_name
   }
 }
-
-resource "mongodbatlas_custom_db_role" "lowest_data_access_possible" {
-  project_id   = var.project_id
-  role_name = "least-possible-data-access-initial-tf-role"
-
-  inherited_roles {
-    role_name = module.mongo_shared.built_in_roles_map.clusterMonitor
-    database_name = local.mongo_admin_db_name
-  }
-}
