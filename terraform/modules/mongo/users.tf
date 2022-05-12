d3b5097ac23589b9b7a6b2d0f2538726a0ea3ae3
Brian Fogg <brian@rair.tech>
2022-05-12 22:39:29 +0000

    Merged in RAIR-3246-create-user-pass-auth-for-bloc (pull request #763)
    RAIR-3246 create user pass auth for blockchain-network user on Mongo Atals
    
    * adding blockchain network user to mongo db tf config
    
    * reeferencing application list object
    
    * fixing references to constants object
    
    * modify to password option
    
    * adding blockchain-user to rairnode
    
    
    Approved-by: Chris Rose

--- terraform/modules/mongo/users.tf
+++ terraform/modules/mongo/users.tf
@@ -1,7 +1,7 @@
 resource "mongodbatlas_database_user" "rairnode" {
   auth_database_name = module.mongo_shared.mongo_external_db_name
   project_id   = var.project_id
-  username = "rairnode"
+  username = local.applications.rairnode.name
   x509_type =  module.mongo_shared.X509Type.MANAGED
 
   roles {
@@ -15,10 +15,29 @@ resource "mongodbatlas_database_user" "rairnode" {
   }
 }
 
+
+resource "mongodbatlas_database_user" "blockchain_network" {
+  auth_database_name = local.mongo_admin_db_name
+  project_id   = var.project_id
+  username = local.applications.blockchain-network.name
+  password = "change_me"
+
+  roles {
+    role_name = mongodbatlas_custom_db_role.read_write_primary_db.role_name
+    database_name = local.mongo_admin_db_name
+  }
+  
+  scopes {
+    name   = var.primary_db_name
+    type = "CLUSTER"
+  }
+}
+
+
 resource "mongodbatlas_database_user" "rairnode_password_option" {
   auth_database_name = local.mongo_admin_db_name
   project_id   = var.project_id
-  username = "rairnode-password-option"
+  username = "${local.applications.rairnode.name}-password-option"
 
   roles {
     role_name = mongodbatlas_custom_db_role.read_write_primary_db.role_name
