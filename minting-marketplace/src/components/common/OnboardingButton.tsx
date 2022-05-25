c8acd333d37bc5e44bab14b44ce4ef4a0afc3235
vvelenteienko <vladyslav@rair.tech>
2022-05-25 14:57:38 +0000

    Merged in feature/RAIR-3263-migrating-onboardingbutton-com (pull request #784)
    feature/RAIR-3263-migrating-onboarding-button
    
    * Merged in dev (pull request #577)
    
    sync in to main
    * Merged dev into feature/RAIR-3263-migrating-onboardingbutton-com
    * added types to onboarding button, added global types file to add type for ethereum entity in global Window object
    
    * Merge branch 'feature/RAIR-3263-migrating-onboardingbutton-com' of bitbucket.org:rairtech/rair into feature/RAIR-3263-migrating-onboardingbutton-com
    
    * Merged dev into feature/RAIR-3263-migrating-onboardingbutton-com
    * Merged dev into feature/RAIR-3263-migrating-onboardingbutton-com
    * Merged dev into feature/RAIR-3263-migrating-onboardingbutton-com
    * Merged dev into feature/RAIR-3263-migrating-onboardingbutton-com
    * Merged dev into feature/RAIR-3263-migrating-onboardingbutton-com
    * Merge branch 'dev', remote-tracking branch 'origin' into feature/RAIR-3263-migrating-onboardingbutton-com-fix-merge-conflict
    
    
    Approved-by: Masha
    Approved-by: Arsenii Pilipenko
    Approved-by: Alexandr Buzetskyi

--- minting-marketplace/src/components/common/OnboardingButton.tsx
+++ minting-marketplace/src/components/common/OnboardingButton.tsx
@@ -1,4 +1,3 @@
-//@ts-nocheck
 import MetaMaskOnboarding from "@metamask/onboarding";
 // import Swal from "sweetalert2";
 import React, { useState, useEffect, useRef, useCallback } from "react";
@@ -8,11 +7,11 @@ const CONNECT_TEXT = "Connect Wallet";
 const CONNECTED_TEXT = "Connected";
 
 export function OnboardingButton() {
-  const [buttonText, setButtonText] = useState(ONBOARD_TEXT);
-  const [isDisabled, setDisabled] = useState(false);
-  const [accounts, setAccounts] = useState([]);
-  const [showButtonPhone, setShowButtonPhone] = useState();
-  const onboarding = useRef();
+  const [buttonText, setButtonText] = useState<string>(ONBOARD_TEXT);
+  const [isDisabled, setDisabled] = useState<boolean>(false);
+  const [accounts, setAccounts] = useState<Array<string>>([]);
+  const [showButtonPhone, setShowButtonPhone] = useState<boolean>();
+  const onboarding = useRef<MetaMaskOnboarding>();
 
   const dappUrl = window.location.host;
   const metamaskAppDeepLink = "https://metamask.app.link/dapp/" + dappUrl;
@@ -44,7 +43,7 @@ export function OnboardingButton() {
       if (accounts.length > 0) {
         setButtonText(CONNECTED_TEXT);
         setDisabled(true);
-        onboarding.current.stopOnboarding();
+        onboarding?.current?.stopOnboarding();
       } else {
         setButtonText(CONNECT_TEXT);
         setDisabled(false);
@@ -71,9 +70,9 @@ export function OnboardingButton() {
     if (MetaMaskOnboarding.isMetaMaskInstalled()) {
       window.ethereum
         .request({ method: "eth_requestAccounts" })
-        .then((newAccounts) => setAccounts(newAccounts));
+        .then((newAccounts: Array<string>) => setAccounts(newAccounts));
     } else {
-      onboarding.current.startOnboarding();
+      onboarding?.current?.startOnboarding();
     }
   };
   return showButtonPhone ? (
