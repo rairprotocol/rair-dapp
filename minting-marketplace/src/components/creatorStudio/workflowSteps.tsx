9a898116f76c94db026080935c7dfdea98823f72
Juan Miguel Sanchez Mola <juan@rair.tech>
2022-05-31 04:03:10 +0000

    Merged in bugfix/RAIR-3340-sort-ranges-by-index (pull request #842)
    Sort offers on the creator flow
    
    * Sort offers on the creator flow
    
    * Merge branch 'dev' into bugfix/RAIR-3340-sort-ranges-by-index
    
    * Merged dev into bugfix/RAIR-3340-sort-ranges-by-index
    
    Approved-by: Arsenii Pilipenko
    Approved-by: Masha

--- minting-marketplace/src/components/creatorStudio/workflowSteps.tsx
+++ minting-marketplace/src/components/creatorStudio/workflowSteps.tsx
@@ -239,6 +239,11 @@ const WorkflowSteps = ({ sentryHistory }) => {
 				}
 			}
 			if (response2?.contract?.product?.offers) {
+				response2.contract.product.offers = response2.contract.product.offers.sort((a, b) => {
+					return a.diamondRangeIndex < b.diamondRangeIndex ? -1 : 1;
+				})
+			}
+			if (response2?.contract?.product?.offers) {
 				let response5 = await rFetch(`/api/nft/network/${response2.contract.blockchain}/${address.toLowerCase()}/${collectionIndex}`);
 				if (response5.success) {
 					response2.contract.nfts = response5.result;		
