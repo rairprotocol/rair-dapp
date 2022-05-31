d83ea31db73a8cc4a0f186cf458dc6d038e467d5
vvelenteienko <vladyslav@rair.tech>
2022-05-31 15:05:13 +0000

    Merged in feature/RAIR-3363-declare-global-union-type-for- (pull request #845)
    Feature/RAIR-3363 declare global union type for
    
    * added global type for blockchain strings
    
    * added one more blockchain string
    
    * minor fix
    
    * Merged dev into feature/RAIR-3363-declare-global-union-type-for-
    * Merged dev into feature/RAIR-3363-declare-global-union-type-for-
    * Merged dev into feature/RAIR-3363-declare-global-union-type-for-
    
    Approved-by: Masha
    Approved-by: tatiana
    Approved-by: Arsenii Pilipenko
    Approved-by: Alexandr Buzetskyi

--- minting-marketplace/src/typescript-global-types/index.d.ts
+++ minting-marketplace/src/typescript-global-types/index.d.ts
@@ -5,4 +5,13 @@ declare global {
     ethereum: MetaMaskInpageProvider;
     dataLayer: Array<object>
   }
+
+// 	BinanceMainnet = '0x38',
+// 	BinanceTestnet = '0x61',
+// 	MaticPolygonTestnet = '0x13881',
+// 	EthereumGoerli = '0x5',
+// 	TEthereumMainnet = '0x1',
+// 	TMaticPolygonMainnet = '0x89'
+//  KlaytnBaobab = '0x3e9'
+  type BlockchainType = '0x38' | '0x61' | '0x13881' | '0x5' | '0x1' | '0x89' | '0x3e9'
 }
