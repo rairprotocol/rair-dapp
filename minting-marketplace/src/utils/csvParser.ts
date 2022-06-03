e8e4a3880eb05d7673320a7defe7ac2ed516f43b
vvelenteienko <vladyslav@rair.tech>
2022-06-03 00:33:09 +0000

    Merged in RAIR-3351-rewright-csvparser (pull request #855)
    Added types to csvParser
    
    * added typing to csvParser
    
    * Merged dev into RAIR-3351-rewright-csvparser
    * Merged dev into RAIR-3351-rewright-csvparser
    * Merged dev into RAIR-3351-rewright-csvparser
    * fixed appeared error
    
    * Merged dev into RAIR-3351-rewright-csvparser
    * Merged dev into RAIR-3351-rewright-csvparser
    
    Approved-by: Masha
    Approved-by: tatiana

--- minting-marketplace/src/utils/csvParser.ts
+++ minting-marketplace/src/utils/csvParser.ts
@@ -1,14 +1,13 @@
-//@ts-nocheck
-const csvParser = (data, resultReceiver, searchColumns, delimiter = ',') => {
+const csvParser = (data: File, resultReceiver: Function, searchColumns?: Array<string>, delimiter = ',') => {
 	const reader = new FileReader();
 	reader.onload = function (e) {
-		const text = e.target.result.split(/\r\n|\n/)
+		const text = (e?.target?.result as string).split(/\r\n|\n/)
 		let headers = text.splice(0, 1)[0].split(delimiter);
 		let getIndexes = Object.keys(headers).map(item => Number(item));
 		if (searchColumns) {
 			getIndexes = searchColumns.map(item => headers.indexOf(item));
 		}
-		let aux = [];
+		let aux: Array<Record<string, unknown>> = [];
 		text.forEach((textItem, textIndex) => {
 			let split = textItem.split(delimiter);
 			split = split.map(item => {
@@ -21,15 +20,15 @@ const csvParser = (data, resultReceiver, searchColumns, delimiter = ',') => {
 					if (item[0] === " ") {
 						split[index - 1 - offset] += delimiter + item;
 						offset++;
-						split[index] = undefined;
+						split[index] = '';
 					}
 				});
-				split = split.filter(item => item !== undefined);
+				split = split.filter(item => item !== '');
 			}
 			if (split.length !== headers.length) {
 				console.error('Error parsing line', textIndex, ', resulted in', split);
 			}
-			let result = {};
+			let result: {[key: string]: string}  = {};
 
 			getIndexes.forEach((item) => {
 				result[headers[item]] = split[item];
