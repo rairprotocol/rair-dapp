ce51814279fd5af325d0ba024a32970c2ffed62d
Chris Rose <chris@rair.tech>
2022-06-10 21:43:30 +0000

    Merged in vaporverse-DNS-update (pull request #914)
    vaporverse-DNS-update
    
    * update configmap for minting-network
    
    * Update DNS for vaporverse
    
    * typo
    
    * Update images and config for nftla
    
    * Update DNS for vaporverse
    
    * fix conflict
    
    * Update DNS for vaporverse
    
    * update to fix conflict
    
    * Merged dev into vaporverse-DNS-update
    * Revert IP

--- terraform/environments/cloudflare/vaporverse_xyz.tf
+++ terraform/environments/cloudflare/vaporverse_xyz.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_record" "vaporverse_xyz__vaporverse_xyz" {
   zone_id = cloudflare_zone.vaporverse_xyz.id
   name    = "@"
-  value   = "34.111.16.215"
+  value   = "34.145.90.106"
   type    = local.record_type.A
 }
