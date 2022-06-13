52a5e3e31861a9f86bd828c3d65fdf90d2570e7e
Chris Rose <chris@rair.tech>
2022-06-13 19:01:29 +0000

    Merged in Update-dns-cloudflare (pull request #922)
    Update dns
    
    * Update dns

--- terraform/environments/cloudflare/vaporverse_xyz.tf
+++ terraform/environments/cloudflare/vaporverse_xyz.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_record" "vaporverse_xyz__vaporverse_xyz" {
   zone_id = cloudflare_zone.vaporverse_xyz.id
   name    = "@"
-  value   = "34.145.90.106"
+  value   = "34.117.79.144"
   type    = local.record_type.A
 }
