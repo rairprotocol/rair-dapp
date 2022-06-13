671136cb9d5acb4c305f9aaf9784168911c0b11b
vvelenteienko <vladyslav@rair.tech>
2022-06-13 15:02:05 +0000

    Merged in feature/RAIR-3280-migrate-main-index.js-from-ts- (pull request #825)
    Feature/RAIR-3280 migrate main index.js from ts
    
    * Merged dev into feature/RAIR-3280-migrate-main-index.js-from-ts-
    * braking changes
    
    * Revert "braking changes"
    
    This reverts commit 8998f2c466fee7b37bb3e058d73ba5e9890bb8ed.
    
    * Merged dev into feature/RAIR-3280-migrate-main-index.js-from-ts-
    * Merged dev into feature/RAIR-3280-migrate-main-index.js-from-ts-
    * Merged dev into feature/RAIR-3280-migrate-main-index.js-from-ts-
    * Merged dev into feature/RAIR-3280-migrate-main-index.js-from-ts-
    * fix typo
    
    * Merge branch 'dev' into feature/RAIR-3280-migrate-main-index.js-from-ts-
    
    * Merged dev into feature/RAIR-3280-migrate-main-index.js-from-ts-
    
    Approved-by: Masha
    Approved-by: Chris Rose
    Approved-by: Alexandr Buzetskyi

--- minting-marketplace/src/index.tsx
+++ minting-marketplace/src/index.tsx
@@ -1,30 +1,32 @@
-//@ts-nocheck
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
 
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
-
+import { init, reactRouterV5Instrumentation } from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import { createBrowserHistory } from "history";
 import { HelmetProvider } from "react-helmet-async";
 import store from "./ducks";
 
 const sentryHistory = createBrowserHistory();
 
+const sentryIoTraceRate = Number(process.env.REACT_APP_SENTRY_IO_TRACE_RATE);
+
 if (process.env.REACT_APP_SENTRY_ENABLED) {
-  Sentry.init({
+  init({
     release: process.env.REACT_APP_SENTRY_RELEASE,
     dsn: process.env.REACT_APP_SENTRY_IO_ENDPOINT,
     integrations: [
-      new Integrations.BrowserTracing({
+      new BrowserTracing({
         routingInstrumentation:
-          Sentry.reactRouterV5Instrumentation(sentryHistory),
-      }),
+          reactRouterV5Instrumentation(sentryHistory),
+      })
     ],
-    tracesSampleRate: process.env.REACT_APP_SENTRY_IO_TRACE_RATE
+    tracesSampleRate: Number.isNaN(sentryIoTraceRate) ?
+     undefined : 
+     sentryIoTraceRate
   });
 }
 
