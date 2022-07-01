d15135ae30a03c76eec96b5c814813d0351feb84
Brian Fogg <brian@rair.tech>
2022-07-01 20:25:01 +0000

    Merged in RAIR-3540-create-staging---prod-dbs-on-m (pull request #988)
    adding DB's for staging and prod
    
    * adding DB's for staging and prod
    
    
    Approved-by: Chris Rose

--- terraform/environments/staging/mongo_db/main.tf
+++ terraform/environments/staging/mongo_db/main.tf
@@ -31,3 +31,32 @@ module "mongo_common" {
   
   project_id = local.project_id
 }
+
+resource "mongodbatlas_cluster" "primary" {
+  project_id   = local.project_id
+  name         = "primary"
+  cluster_type = module.mongo_shared.cluster_type_map.REPLICASET
+
+  replication_specs {
+    num_shards = 1
+    regions_config {
+      region_name     = module.mongo_shared.gcp_region_map.us-central1
+      electable_nodes = 3
+      priority        = 7
+      read_only_nodes = 0
+    }
+  }
+
+  mongo_db_major_version       = "5.0"
+  version_release_system       = module.mongo_shared.version_release_system_map.LTS
+  paused                       = false
+  cloud_backup                 = true
+  auto_scaling_disk_gb_enabled = false
+  auto_scaling_compute_enabled = false
+  auto_scaling_compute_scale_down_enabled = false
+
+  # Provider Settings "block"
+  provider_name               = module.mongo_shared.gcp_provider_name
+  disk_size_gb                = 10
+  provider_instance_size_name = module.mongo_shared.instance_size_map.M10.name
+}
