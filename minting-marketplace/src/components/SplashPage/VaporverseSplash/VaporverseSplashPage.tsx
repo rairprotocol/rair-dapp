fef5f0ab734d48f83f11f1bd10597dea79469dc1
Martin Adams <martin.adams@rair.tech>
2022-09-07 19:26:04 +0000

    Merged in bugfix/RAIR-3613-vaporverse-needs-help-button-i (pull request #1119)
    removed references to UkraineGlitch and unnecessary splashpage data
    
    * removed references to UkraineGlitch and unnecessary splashpage data
    
    * Merge branch 'dev' into bugfix/RAIR-3613-vaporverse-needs-help-button-i
    
    * Merge branch 'dev' into bugfix/RAIR-3613-

--- minting-marketplace/src/components/SplashPage/VaporverseSplash/VaporverseSplashPage.tsx
+++ minting-marketplace/src/components/SplashPage/VaporverseSplash/VaporverseSplashPage.tsx
@@ -16,11 +16,7 @@ import VV_warning_1 from '../images/vv_warning_1.png';
 import VV_warning_2 from '../images/vv_warning_2.png';
 import VV_test_transmission from '../images/vv_test_transmission.png';
 import favion_Vaporverse from './../images/favicons/vv_Rair_logo.ico';
-import UKR126 from '../images/UkraineGlitchNFTExamples/126.jpg';
-import UKR497 from '../images/UkraineGlitchNFTExamples/497.jpg';
-import UKR1294 from '../images/UkraineGlitchNFTExamples/1294.jpg';
 import videoBackground from '../images/vaporverse_video_background.png';
-import nftCountUkraine from '../images/UkrainianSoldierswithMedical/nftCountUkraine.jpeg';
 
 /* importing Components*/
 import TeamMeet from '../TeamMeet/TeamMeetList';
@@ -32,48 +28,6 @@ import MetaTags from '../../SeoTags/MetaTags';
 import ModalHelp from '../SplashPageTemplate/ModalHelp';
 import PurchaseChecklist from '../PurchaseChecklist/PurchaseChecklist';
 import { setRealChain } from '../../../ducks/contracts/actions';
-
-// TODO: UNUSED
-// import { rFetch } from '../../../utils/rFetch';
-// import PurchaseTokenButton from '../../common/PurchaseToken';
-// import Swal from 'sweetalert2';
-// import TokenLeftTemplate from '../TokenLeft/TokenLeftTemplate';
-// import StaticTiles from "../SplashPageTemplate/VideoTiles/StaticTiles";
-// import UnlockableVideo from "../images/nipsey1.png";
-// import NFTCounter from "../SplashPageTemplate/NFTCounter/NFTCounter";
-// import CarouselModule from '../SplashPageTemplate/Carousel/Carousel';
-// import setTitle from '../../../utils/setTitle';
-// import faviconUkraine from './../images/favicons/favicon-ukraine.ico';
-// import NFTLA_Video from "../images/NFT-LA-RAIR-2021.mp4";
-// import MetaMaskIcon from '../images/metamask_logo.png';
-// import DiscordIcon from '../images/discord-icon.png';
-// import UKR1518 from '../images/UkraineGlitchNFTExamples/1518.jpg';
-// import UKR1641 from '../images/UkraineGlitchNFTExamples/1641.jpg';
-// import UKR1896 from '../images/UkraineGlitchNFTExamples/1896.jpg';
-// import UKR653 from '../images/UkraineGlitchNFTExamples/653.jpg';
-// import UKR777 from '../images/UkraineGlitchNFTExamples/777.jpg';
-// import UKR1050 from '../images/UkraineGlitchNFTExamples/1050.jpg';
-// import UKR246 from '../images/UkraineGlitchNFTExamples/246.jpg';
-
-// Google Analytics
-//const TRACKING_ID = 'UA-209450870-5'; // YOUR_OWN_TRACKING_ID
-//ReactGA.initialize(TRACKING_ID);
-
-// This will be the default contract used in this splash page
-// // TODO: UNUSED
-// const mainContract = {
-//   contractAddress: '0xbd034e188f35d920cf5dedfb66f24dcdd90d7804',
-//   requiredBlockchain: '0x1',
-//   offerIndex: [0, 1]
-// };
-
-// By setting REACT_APP_TEST_CONTRACTS
-// TODO: UNUSED
-// const testContract = {
-//   contractAddress: '0x971ee6dd633cb6d8cc18e5d27000b7dde30d8009',
-//   requiredBlockchain: '0x5',
-//   offerIndex: [52, 0]
-// };
 
 const splashData = {
   title: null,
@@ -112,32 +66,6 @@ const splashData = {
     buttonImg: null,
     buttonLink: 'https://twitter.com/rairtech'
   },
-
-  exclusiveNft: {
-    title: '',
-    titleColor: 'rgb(189,52,183)'
-  },
-  carouselTitle: 'Our Developers',
-  carouselData: [
-    {
-      title: 'Alex & Arsenii',
-      img: UKR126,
-      description:
-        'Originally based in Kharkiv, Alex and Arsenii are experienced frontend REACT developers that created the site you see here'
-    },
-    {
-      title: 'Masha',
-      img: UKR497,
-      description:
-        'Born in Odessa, Masha emigrated with her family to the US when she was a small child. They remained closely connected to the Post-Soviet immigrant community of the Pacific NW while growing up. She created these pieces of glitch art as a way to process the grief of a country in dispair, and to continue funding the shipment of medical supplies to the war-torn area.'
-    },
-    {
-      title: 'Valerii & Natalia',
-      img: UKR1294,
-      description:
-        'Originally based in Kharkiv, Valerii and Natalia are respsonsible for backend database architecture and syncing to allow for seemless purchase'
-    }
-  ],
   videoDataDemo: {
     video: null,
     // 'https://storage.googleapis.com/rair-videos/tx2cV7kzqFXF9lTC5iy1VCYoXBwonyG-HcjunEI5j1rqfX/2596768157',
@@ -157,51 +85,7 @@ const splashData = {
     mediaId: 'tx2cV7kzqFXF9lTC5iy1VCYoXBwonyG-HcjunEI5j1rqfX'
   },
   tilesTitle: null,
-  NFTName: 'NFT',
-  counterData: {
-    titleColor: '#FFD505',
-    title1: '#ukraineglitch',
-    title2: 'origins',
-    backgroundImage: `url(${nftCountUkraine})`,
-    btnColorIPFS: '#035BBC',
-    nftCount: 1991,
-    royaltiesNft: {
-      firstBlock: [
-        'Ukrainian Dev Fund: 42%',
-        'Medical Supplies: 42%',
-        'RAIR Node Fee: 10%',
-        'Contract Gas Fund: 6%'
-      ],
-      secondBlock: [
-        '1 UkraineGlitch Flag Supports',
-        '- weeks of lodging for 1 Ukrainian',
-        '- 10 Tourniquets sent to those in need'
-      ]
-    },
-    properties: [
-      {
-        titleProperty: 'Fermion Freckles',
-        titleColor: '#FFD505',
-        propertyDesc: '14',
-        percent: '32%'
-      },
-      {
-        titleProperty: 'Boson Movement',
-        titleColor: '#035BBC',
-        propertyDesc: 'Sparklink / Still',
-        percent: '1.7%'
-      }
-    ],
-    description: [
-      'This (de)generative art project was created to provide 100% direct aid to Ukraine.',
-      '2014',
-      'Ukrainian immigrants in the Pacific Northwest of the United States had taken it into their own hands to provide a direct supply chain of specialty tactical medical equipment to Ukraine’s grassroots militias. We are here to continue their story.',
-      '2022',
-      'This website was built by Ukrainian Web3 developers. The logic of the algorithm written by a Ukranian generative artist. Prior to the war they were making NFTs for fun, now for a purpose.',
-      'About the piece',
-      'The algorithm is set such that as the number of the NFT increases, the chaotic opposite square masses also increase, eventually inverting or turning the flag upside-down. Some NFTs are animated (sparkling) to show parts of this progression, and others are still. Only holders of a UkraineGlitch NFT can watch the full animation of the flag transformation occur below.'
-    ]
-  }
+  NFTName: 'NFT'
 };
 
 const Text = ({ text }) => {
@@ -233,7 +117,7 @@ const VaporverseSplashPage = ({ connectUserData, setIsSplashPage }) => {
   const carousel_match = window.matchMedia('(min-width: 630px)');
   const [carousel, setCarousel] = useState(carousel_match.matches);
   const [purchaseList, setPurchaseList] = useState(true);
-  const ukraineglitchChainId = '0x1';
+  const chainId = '0x1';
   const dispatch = useDispatch();
 
   const togglePurchaseList = () => {
@@ -262,7 +146,7 @@ const VaporverseSplashPage = ({ connectUserData, setIsSplashPage }) => {
   // }, [getAllProduct])
 
   useEffect(() => {
-    dispatch(setRealChain(ukraineglitchChainId));
+    dispatch(setRealChain(chainId));
   }, [dispatch]);
 
   useEffect(() => {
