6e496fd9405d6a9eda7426a81c837ea1982c5598
Micahel Bielikov <michael@rair.tech>
2022-09-19 23:09:37 +0000

    Merged in RAIR-3721-remove-product.category-field- (pull request #1239)
    RAIR-3721 Removed Product.category
    
    * Merge branch 'dev' of bitbucket.org:rairtech/rair into dev
    
    * Merge branch 'dev' of bitbucket.org:rairtech/rair into dev
    
    * Merge branch 'dev' of bitbucket.org:rairtech/rair into dev
    
    * Merge branch 'dev' of bitbucket.org:rairtech/rair into dev
    
    * Merge branch 'dev' of bitbucket.org:rairtech/rair into dev
    
    * Merge branch 'dev' of bitbucket.org:rairtech/rair into dev
    
    * Removed Product.category
    
    * Merge branch 'dev' of bitbucket.org:rairtech/rair into dev
    
    * Merge branch 'dev' of bitbucket.org:rairtech/rair into dev
    
    * Merge branch 'dev' into RAIR-3721-remove-product.category-field-
    
    
    Approved-by: Valerii Kovalov
    Approved-by: dima

--- rairnode/bin/schemas/filterAndSort.js
+++ rairnode/bin/schemas/filterAndSort.js
@@ -1,13 +1,11 @@
 const Joi = require('joi');
 
 module.exports = Joi.object({
-  pageNum: Joi.number()
-    .min(1),
-  itemsPerPage: Joi.number()
-    .min(5),
+  pageNum: Joi.number().min(1),
+  itemsPerPage: Joi.number().min(5),
   blockchain: Joi.string()
     .pattern(/^0x\w*/)
     .messages({ 'string.pattern.base': 'Invalid blockchain hash' }),
-  category: Joi.string()
-    .min(1),
+  // TODO: remove on V2
+  category: Joi.string().min(1),
 });
