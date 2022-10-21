7ded60d3e7c21fb62029cc8a066b074e750f5ee8
Volodymyr Bogryashov <vova@rair.tech>
2022-10-21 14:41:54 +0000

    Merged in bugfix/RAIR-3883-fix-search-filed-pop-up (pull request #1346)
    Bugfix/RAIR-3883 fix search filed pop up
    
    * RAIR-3875-fix-main-search
    
    * Merge branch 'dev' of https://bitbucket.org/rairtech/rair into bugfix/RAIR-3883-fix-search-filed-pop-up
    
    * RAIR-3875-fix-main-search
    
    * RAIR-3875-fix-main-search
    
    * Merge branch 'dev' of https://bitbucket.org/rairtech/rair into bugfix/RAIR-3883-fix-search-filed-pop-up
    
    * Bugfix/RAIR-3883 fix search filed pop up
    
    * Merged dev into bugfix/RAIR-3883-fix-search-filed-pop-up
    * Merged dev into bugfix/RAIR-3883-fix-search-filed-pop-up
    * Merged dev into bugfix/RAIR-3883-fix-search-filed-pop-up
    
    Approved-by: Arsenii Pilipenko
    Approved-by: Alexandr Buzetskyi

---
+++ minting-marketplace/src/hooks/useComponentVisible.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef, useState } from 'react';
+
+function useComponentVisible(initialIsVisible: any) {
+  const [isComponentVisible, setIsComponentVisible] =
+    useState<boolean>(initialIsVisible);
+  const ref = useRef<any>(null);
+
+  const handleHideDropdown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      setIsComponentVisible(false);
+    }
+  };
+
+  const handleClickOutside = (event: { target: any }) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setIsComponentVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleHideDropdown, true);
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('keydown', handleHideDropdown, true);
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  });
+
+  return { ref, isComponentVisible, setIsComponentVisible };
+}
+
+export default useComponentVisible;
