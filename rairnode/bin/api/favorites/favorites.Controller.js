74ddd1feec2cbfb71e1ce01b4cc93ea18f44a9cc
Juan Miguel Sanchez Mola <juan@rair.tech>
2023-04-26 15:58:13 +0000

    Merged in feature/RAIR-4230-user-specific-favorites (pull request #1574)
    RAIR-4230 - User-specific Favorites API endpoint
    
    * New endpoint
    
    * Merged dev into feature/RAIR-4230-user-specific-favorites
    
    Approved-by: Masha

---
+++ rairnode/bin/api/favorites/favorites.Controller.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const {
+    createFavorite,
+    getAllFavoritesByUser,
+    getAllFavoritesOfAddress,
+    deleteFavorite,
+} = require('./favoriteTokens.Service');
+const { validation, isOwner, requireUserSession } = require('../../middleware');
+const { FavoriteTokens } = require('../../models');
+
+const router = express.Router();
+router.post('/', requireUserSession, validation(['createFavoriteToken']), createFavorite);
+router.get('/', requireUserSession, validation(['pagination'], 'query'), getAllFavoritesByUser);
+router.get(
+    '/:userAddress',
+    validation(['pagination', 'userAddress'], 'query'),
+    getAllFavoritesOfAddress,
+);
+router.delete('/:id', requireUserSession, validation(['dbId'], 'params'), isOwner(FavoriteTokens), deleteFavorite);
+
+module.exports = router;
