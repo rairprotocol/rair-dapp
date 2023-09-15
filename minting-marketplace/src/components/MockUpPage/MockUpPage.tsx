f083a0513c42e2df6f9d264185fffb376601eae0
Sean Michaels <sean@rair.tech>
2023-09-15 20:07:26 +0000

    Merged in bugfix/RAIR-4524-handle-feature-banner-in-case- (pull request #1767)
    Handle feature banner in case Profile photo is not uploaded by the owner of the collection
    
    * fix an issue, styles and clear component
    
    * Merged dev into bugfix/RAIR-4524-handle-feature-banner-in-case-
    
    Approved-by: Masha

--- minting-marketplace/src/components/MockUpPage/MockUpPage.tsx
+++ minting-marketplace/src/components/MockUpPage/MockUpPage.tsx
@@ -1,6 +1,5 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 
 import SearchPanel from './SearchPanel';
 
@@ -8,12 +7,9 @@ import SearchPanel from './SearchPanel';
 import { RootState } from '../../ducks';
 import { ColorStoreType } from '../../ducks/colors/colorStore.types';
 import { setShowSidebarTrue } from '../../ducks/metadata/actions';
-import { defaultAvatar, hotDropsDefaultBanner } from '../../images';
-import chainData from '../../utils/blockchainData';
 import { rFetch } from '../../utils/rFetch';
 
-import { ImageLazy } from './ImageLazy/ImageLazy';
-import { changeIPFSLink } from './NftList/utils/changeIPFSLink';
+import MainBanner from './MainBanner/MainBanner';
 import { IMockUpPage } from './SelectBox/selectBox.types';
 
 const MockUpPage: React.FC<IMockUpPage> = ({ tabIndex, setTabIndex }) => {
@@ -22,9 +18,6 @@ const MockUpPage: React.FC<IMockUpPage> = ({ tabIndex, setTabIndex }) => {
   const { primaryColor, textColor } = useSelector<RootState, ColorStoreType>(
     (store) => store.colorStore
   );
-  const navigate = useNavigate();
-
-  const hotdropsVar = process.env.REACT_APP_HOTDROPS;
 
   const getCollectionBanner = async () => {
     const response = await rFetch(`/api/settings/featured`);
@@ -32,14 +25,6 @@ const MockUpPage: React.FC<IMockUpPage> = ({ tabIndex, setTabIndex }) => {
       setMainBannerInfo(response.data);
     }
   };
-
-  const goMainCollection = useCallback(() => {
-    if (mainBannerInfo) {
-      navigate(
-        `/collection/${mainBannerInfo.blockchain}/${mainBannerInfo.contract}/${mainBannerInfo.product}/0`
-      );
-    }
-  }, [mainBannerInfo, navigate]);
 
   useEffect(() => {
     dispatch(setShowSidebarTrue());
@@ -55,53 +40,7 @@ const MockUpPage: React.FC<IMockUpPage> = ({ tabIndex, setTabIndex }) => {
 
   return (
     <div className={'mock-up-page-wrapper'}>
-      <div
-        className={`collection-background main-banner`}
-        onClick={() => goMainCollection()}>
-        <ImageLazy
-          className="picture-banner"
-          alt="Collection Banner"
-          src={
-            mainBannerInfo && mainBannerInfo?.collectionBanner
-              ? `${changeIPFSLink(mainBannerInfo?.collectionBanner)}`
-              : hotdropsVar === 'true'
-              ? hotDropsDefaultBanner
-              : 'https://storage.googleapis.com/rair_images/1683038949498-1548817833.jpeg'
-          }
-        />
-        {mainBannerInfo && (
-          <div className="collection-background-main-info">
-            <div className="collection-info-contract">
-              <div className="collection-info-product-name">
-                {mainBannerInfo.collectionName}
-              </div>
-              {mainBannerInfo && mainBannerInfo.user && (
-                <div className="collection-info-user">
-                  {mainBannerInfo && mainBannerInfo.user ? (
-                    <img src={mainBannerInfo.user.avatar} alt="avatar" />
-                  ) : (
-                    <img src={defaultAvatar} alt="avatar" />
-                  )}
-                  <div>
-                    {mainBannerInfo && mainBannerInfo.user
-                      ? mainBannerInfo.user.nickName
-                      : '123'}
-                  </div>
-                </div>
-              )}
-            </div>
-            {mainBannerInfo.blockchain && (
-              <div className="collection-info-blockchain">
-                <img
-                  src={chainData[mainBannerInfo.blockchain]?.image}
-                  alt="blockchain"
-                />
-                {chainData[mainBannerInfo.blockchain]?.symbol}
-              </div>
-            )}
-          </div>
-        )}
-      </div>
+      <MainBanner mainBannerInfo={mainBannerInfo} />
       <SearchPanel
         tabIndex={tabIndex}
         setTabIndex={setTabIndex}
