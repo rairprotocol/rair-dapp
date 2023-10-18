28489554d1a33e5adc3faab64feae1d66221306b
Sean Michaels <sean@rair.tech>
2023-10-18 14:06:54 +0000

    Merged in bugfix/RAIR-4578-remove-closed-beta-header-text (pull request #1806)
    RAIR-4578 - Remove closed beta text from main page
    
    * Remove closed beta text from main page
    
    * Merged dev into bugfix/RAIR-4578-remove-closed-beta-header-text
    
    Approved-by: Juan Miguel Sanchez Mola

--- minting-marketplace/src/components/FrontPage/WelcomeHeader.tsx
+++ minting-marketplace/src/components/FrontPage/WelcomeHeader.tsx
@@ -28,20 +28,6 @@ const WelcomeHeader = ({
   return (
     <div className="main-wrapper">
       <MetaTags seoMetaTags={seo} />
-      {process.env.REACT_APP_HOTDROPS === 'true' ? (
-        <></>
-      ) : (
-        <div className="col-6 text-left main">
-          <h1 className="w-100 general-title">
-            RAIR CLOSED <b className="title">BETA</b>
-          </h1>
-          <p className="w-100 general-title">
-            This marketplace is currently in closed beta access. Contact us for
-            a RAIR Genesis Pass if you would like to test our tools. Mint NFTs,
-            Create Metadata, Upload token-gated content, Collect Analytics.
-          </p>
-        </div>
-      )}
       <div className="col-12 mt-3 row">
         <MockUpPage tabIndex={tabIndex} setTabIndex={setTabIndex} />
       </div>
