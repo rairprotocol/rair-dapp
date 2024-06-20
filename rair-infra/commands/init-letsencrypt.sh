54f01952430191b4276c4b3cccf772606805a2b8
Ubuntu <azureuser@hackathon-1.cib2dyxen3yudfxiortyntaldf.rx.internal.cloudapp.net>
2024-06-20 08:19:30 +0000

    new files for ssl config

---
+++ rair-infra/commands/init-letsencrypt.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+domains=(example.org www.example.org)
+rsa_key_size=4096
+data_path="./data/certbot"
+email="" # Adding a valid address is strongly recommended
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+
+if [ -d "$data_path" ]; then
+  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+    exit
+  fi
+fi
+
+
+if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
+  echo "### Downloading recommended TLS parameters ..."
+  mkdir -p "$data_path/conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+  echo
+fi
+
+echo "### Creating dummy certificate for $domains ..."
+#path="/etc/letsencrypt/live/$domains"
+mkdir -p "$data_path/conf/live/$domains"
+path="$data_path/conf/live/$domains"
+echo $path
+openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+    -keyout "$path/privkey.pem" \
+    -out "$path/fullchain.pem" \
+    -subj "/CN=wallstreet80sclub.xyz"
+
