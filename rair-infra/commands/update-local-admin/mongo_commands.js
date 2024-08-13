57697eb9eed7ca2ab1f55bc15abe7d7c317595ff
Suresh Arora <suresh_arora@hotmail.com>
2024-08-13 14:15:55 +0530

    Update mongo_commands.js
    Added default Base Blockchain

--- rair-infra/commands/update-local-admin/mongo_commands.js
+++ rair-infra/commands/update-local-admin/mongo_commands.js
@@ -7,6 +7,22 @@ db = db.getSiblingDB('admin')
 db.auth("admin","admin")
 db = db.getSiblingDB('rair-db')
 
+//Set Blockchain RPC Url
+db.Blockchain.insertOne({
+   "hash": "0x2105",
+   "name": "Base",
+   "display": "true",
+   "sync": "true",
+   "alchemySupport": "false",
+   "blockExplorerGateway": "https://basescan.org",
+   "mainTokenAddress": "0x2b0fFbF00388f9078d5512256c43B983BB805eF8",
+   "diamondFactoryAddress": "0x1F89Cc515dDc53dA2fac5B0Ca3b322066A71E6BA",
+   "diamondMarketplaceAddress": "0x58795f50b50d492C4252B9BBF78485EF4043FF3E",
+   "numericalId": "8453",
+   "rpcEndpoint": "https://base.meowrpc.com/",
+   "symbol": "bETH"
+})
+
 
 //Set SuperAdmin
 db.ServerSetting.updateMany({},
