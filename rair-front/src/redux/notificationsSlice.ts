7e57118bf41e656848a6738e16f641d53ac44c46
SeanM322 <sean@rair.tech>
2024-10-01 17:54:26 +0300

    format fix

--- rair-front/src/redux/notificationsSlice.ts
+++ rair-front/src/redux/notificationsSlice.ts
@@ -5,83 +5,85 @@ import axios from 'axios';
 import { dataStatuses } from './commonTypes';
 
 interface NotificationState {
-    notifications: Array<any>;
-    totalCount: number;
-    totalUnreadCount: number;
-    notificationsStatus: dataStatuses;
+  notifications: Array<any>;
+  totalCount: number;
+  totalUnreadCount: number;
+  notificationsStatus: dataStatuses;
 }
 
 interface NotificaitonItem {
-    createdAt: string;
-    data: Array<any>;
-    read: boolean;
-    tokenData: Array<any>;
-    type: string;
-    updatedAt: string;
-    user: string;
-    _id: string;
+  createdAt: string;
+  data: Array<any>;
+  read: boolean;
+  tokenData: Array<any>;
+  type: string;
+  updatedAt: string;
+  user: string;
+  _id: string;
 }
 
 interface NotificationResponse {
-    totalCount: number;
-    success: boolean;
-    notifications: Array<NotificaitonItem>;
+  totalCount: number;
+  success: boolean;
+  notifications: Array<NotificaitonItem>;
 }
 
 const initialState: NotificationState = {
-    notifications: [],
-    totalCount: 0,
-    totalUnreadCount: 0,
-    notificationsStatus: dataStatuses.Uninitialized
+  notifications: [],
+  totalCount: 0,
+  totalUnreadCount: 0,
+  notificationsStatus: dataStatuses.Uninitialized
 };
 
 export const fetchNotifications = createAsyncThunk(
-    'notifications/fetchData',
-    async(pageNum?: number) => {
-        const responseData = await axios.get(
-            `/api/notifications${pageNum ? `?pageNum=${Number(pageNum)}` : ''}`
-          );
+  'notifications/fetchData',
+  async (pageNum?: number) => {
+    const responseData = await axios.get(
+      `/api/notifications${pageNum ? `?pageNum=${Number(pageNum)}` : ''}`
+    );
 
-        const responseUnreadData = await axios.get(`/api/notifications?onlyUnread=true`);
+    const responseUnreadData = await axios.get(
+      `/api/notifications?onlyUnread=true`
+    );
 
-
-
-        return {
-            data: responseData,
-            unreadData: responseUnreadData
-        };
-    }
-)
+    return {
+      data: responseData,
+      unreadData: responseUnreadData
+    };
+  }
+);
 
 export const notificationsSlice = createSlice({
   name: 'notifications',
   initialState,
   reducers: {
     clearResults: (state) => {
-        state.notificationsStatus = {};
-      },
+      state.notificationsStatus = {};
+    }
   },
   extraReducers: (builder) => {
-    builder.addCase(fetchNotifications.pending, (state) => {
+    builder
+      .addCase(fetchNotifications.pending, (state) => {
         state.notificationsStatus = dataStatuses.Loading;
-    })
-    .addCase(
+      })
+      .addCase(
         fetchNotifications.fulfilled,
         (state, action: PayloadAction<NotificationResponse>) => {
           state.notificationsStatus = dataStatuses.Complete;
-          const sortedNotifications = action.payload.data.data.notifications.sort((a, b) => {
-            if (!a.read && b.read) return -1;
-            if (a.read && !b.read) return 1;
+          const sortedNotifications =
+            action.payload.data.data.notifications.sort((a, b) => {
+              if (!a.read && b.read) return -1;
+              if (a.read && !b.read) return 1;
 
-            const dateA = new Date(a.createdAt).getTime();
-            const dateB = new Date(b.createdAt).getTime();
+              const dateA = new Date(a.createdAt).getTime();
+              const dateB = new Date(b.createdAt).getTime();
 
-            return dateB - dateA;
-          });
+              return dateB - dateA;
+            });
 
           state.notifications = sortedNotifications;
           state.totalCount = action.payload.data.data.totalCount;
-          state.totalUnreadCount = action.payload.unreadData.data.totalCount
+          state.totalUnreadCount = action.payload.unreadData.data.totalCount;
         }
       )
       .addCase(fetchNotifications.rejected, (state) => {
@@ -92,4 +94,3 @@ export const notificationsSlice = createSlice({
 
 export const { clearResults } = notificationsSlice.actions;
 export default notificationsSlice.reducer;
-
