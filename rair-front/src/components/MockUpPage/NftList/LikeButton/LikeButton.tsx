ea3d25b8ed6ec63f835dba0e46c83087ea2f8e29
Juan Miguel Sanchez Mola <jmsm412@gmail.com>
2024-10-22 10:18:00 -0400

    Message if not logged in

--- rair-front/src/components/MockUpPage/NftList/LikeButton/LikeButton.tsx
+++ rair-front/src/components/MockUpPage/NftList/LikeButton/LikeButton.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useReducer } from 'react';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { CircularProgress } from '@mui/material';
 import axios from 'axios';
