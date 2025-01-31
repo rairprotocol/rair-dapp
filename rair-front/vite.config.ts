4fa218ac18fd106d4bb77cf33329719666dcd1ff
Juan Miguel Sanchez Mola <jmsm412@gmail.com>
2025-01-31 09:00:56 -0400

    Package bump

--- rair-front/vite.config.ts
+++ rair-front/vite.config.ts
@@ -6,7 +6,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
-    include: ['@emotion/react', '@emotion/styled', '@mui/material/Tooltip']
+    include: ['@mui/material/Tooltip']
   },
   plugins: [react(), nodePolyfills(), svgr()],
   server: {
